8304afb124266f6b08f06f70277972ce03d35dd7
iagorrr04 <iagorrr04@gmail.com>
2022-07-10 18:32:26 -0300

    new

--- Codeforces/1466C.cpp
+++ Codeforces/1466C.cpp
@@ -4,32 +4,32 @@
 using namespace std;
 
 // Já que usa so letra minuscula substituir por qualquer um ae e é isso.
-int solve(string S){
-    int ans = 0;
-    for(int l = 0; l < S.size(); l++){
-        int r = 1;
-        while(r < 3 && l+r < S.size()){
-            if(S[l] == S[l+r] && S[l] != 'X'){
-                ans++;
-                S[l+r] = 'X';
-            }
-            r++;
-        }
+int solve(string S) {
+  int ans = 0;
+  for (int l = 0; l < S.size(); l++) {
+    int r = 1;
+    while (r < 3 && l + r < S.size()) {
+      if (S[l] == S[l + r] && S[l] != 'X') {
+        ans++;
+        S[l + r] = 'X';
+      }
+      r++;
     }
+  }
 
-    return ans;
+  return ans;
 }
 
-int main(){
-    int T;
-    cin >> T;
+int main() {
+  int T;
+  cin >> T;
 
-    while(T--){
-        string S;
-        cin >> S;
-        cout << solve(S) << endl;
-    }
-    return 0;
+  while (T--) {
+    string S;
+    cin >> S;
+    cout << solve(S) << endl;
+  }
+  return 0;
 }
 
 // Accepted.
