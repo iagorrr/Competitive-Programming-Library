56cfbd85dcc7036956649b2313868612b9c4dfec
iagorrr04 <iagorrr04@gmail.com>
2022-08-24 14:43:18 -0300

    correct version

--- algorithms/knapsack_dp.cpp
+++ algorithms/knapsack_dp.cpp
@@ -7,6 +7,7 @@
 */
 #include <bits/stdc++.h>
 #define MAX_N 10000
+#define MAX_M 10000
 using namespace std;
 using ll = long long;
 using vll = vector<long long>;
@@ -14,118 +15,81 @@ using vll = vector<long long>;
 /*
    decrease the second parameter of the dp, to verify if is possible to add the element...
 */
-ll w[MAX_N+1], v[MAX_N+1];
-ll n, s;
-ll knapsack_dp(){
-    // dp initialization.
-    ll INF = LLONG_MAX;
-    ll dp[MAX_N+1][s+1];
 
-    dp[0][0] = 0;
-    for(ll i = 1; i <= s; ++i) dp[0][i] = -INF;
-    
-    // calcucating the best options.
-    for(ll i = 1; i <= n; ++i){
-        for(ll j = 0; j <= s; ++j){
-            // can't choose this element.
-            if(w[i] > j) dp[i][j] = dp[i-1][j];
-            else{
-                if(dp[i-1][j] <= dp[i-1][j -w[i]]+v[i]){// choose this element.
-                    dp[i][j] = dp[i-1][j-w[i]] + v[i];
-                }
-                else{// don't choose this element.
-                    dp[i][j] = dp[i-1][j];
-                }
+ll dp[MAX_N][MAX_M];
+bool check[MAX_N][MAX_M];
+
+pair<ll, vector<int>> knapsack(int M, const vector<pair<ll,ll>>& cs)
+{
+    int N = (int) cs.size() - 1;  // Os elementos começam em 1
+
+    // Casos-base
+    for (int i = 0; i <= N; ++i)
+        dp[i][0] = 0;
+
+    for (int m = 0; m <= M; ++m)
+        dp[0][m] = 0;
+
+    // Transições
+    for (int i = 1; i <= N; ++i)
+    {
+        for (int m = 1; m <= M; ++m)
+        {
+            dp[i][m] = dp[i - 1][m];
+            check[i][m] = false;
+
+            auto [w, v] = cs[i];
+
+            if (w <= m and dp[i - 1][m - w] + v > dp[i][m])
+            {
+                dp[i][m] = dp[i - 1][m - w] + v;
+                check[i][m] = true;
             }
         }
     }
-    
-    // finding the answer.
-    ll ans = 0;
-    for(ll i = 1; i <= s; ++i)
-        if(ans < dp[n][i]) ans = dp[n][i];
-    
 
+    // Recuperação dos elementos
+    int m = M;
+    vector<int> is;
 
-    return ans;
+    for (int i = N; i >= 1; --i)
+    {
+        if (check[i][m])
+        {
+            is.push_back(i);
+            m -= cs[i].first;
+        }
+    }
 
+    reverse(is.begin(), is.end());
+
+    return { dp[N][M], is };
 }
 
-/*
-    Almost the same but in this way restore the elements choosen. 
-*/
-ll knapsack_dp_elements(){
-    // choosed elements.
-    bool opt[MAX_N+1][s+1];
-
-    // dp initialization.
-    ll INF = LLONG_MAX;
-    ll dp[MAX_N+1][s+1];
-    dp[0][0] = 0;
-    for(ll i = 1; i <= s; ++i) dp[0][i] = -INF;
-    
-    // calcucating the best options.
-    for(ll i = 1; i <= n; ++i){
-        for(ll j = 0; j <= s; ++j){
-            // can't choose this element.
-            if(w[i] > j){
-                opt[i][j] = false;
-                dp[i][j] = dp[i-1][j];
-            }
-            else{
-                if(dp[i-1][j] <= dp[i-1][j -w[i]]+v[i]){// choose this element.
-                    opt[i][j] = true;
-                    dp[i][j] = dp[i-1][j-w[i]] + v[i];
-                }
-                else{// don't choose this element.
-                    opt[i][j] = false;
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-    }
-    
-    // finding the answer.
-    ll s2 = 0;
-    ll ans = 0;
-    for(ll i = 1; i <= s; ++i){
-        if(ans < dp[n][i]){
-            s2 = i;
-            ans = dp[n][i];
-        }
-    }
-
-    // finding the best sequence.
-    vll items;
-    for(ll i = n; i > 0; --i){
-        if(opt[i][s2]){// the element was used.
-            s -= w[i];
-            items.push_back(i);        
-        }
-    }
-    reverse(items.begin(), items.end());
-    cout << "items at the maximum sum : ";
-    for(auto x : items) cout << x << " ";
-    cout << endl;
-
-    return ans;
-}
-
-int main(){
+int main()
+{   
     srand(time(NULL));
-
-    cout << "total items : ";
-    cin >>  n;
-    cout << "max weight : ";
-    cin >> s;
-    for(ll i = 1; i <= n; ++i){
-        v[i] = rand()%100;
-        w[i] = rand()%100;
-        cout << "w: " << w[i] << " v: " << v[i] << endl;    
-    }
     
-    cout << knapsack_dp_elements() << endl;
-    cout << knapsack_dp() << endl;
+    ll m, n;
+    cout << "total elements : ";
+    cin >> n;
+    cout << "max weight : ";
+    cin >> m;
+    vector<pair<ll, ll>> cs(n );
+    for(ll i = 0; i < n; ++i){
+        cs[i].first = rand()%10;
+        cs[i].second = rand()%10;
+        cout << "[" << cs[i].first << "," << cs[i].second  <<"] ";
+    }
+    cout << endl;
+    auto [ans, is] = knapsack(m, cs);
+
+    cout << "max value: " << ans << '\n';
+
+    cout << "elements: ";
+    for (auto i : is)
+        cout << i << ' ';
+    cout << '\n';
 
     return 0;
 }
