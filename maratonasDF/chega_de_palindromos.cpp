6caa7cb99409112e442b7031279b641c62e42756
iagorrr04 <iagorrr04@gmail.com>
2022-09-18 12:27:50 -0300

    :white_check_mark: questões do maratonas DF adicionadas

---
+++ maratonasDF/chega_de_palindromos.cpp
@@ -0,0 +1,25 @@
+// iagorrr
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+/*
+    Abordagem gulosa
+
+    1: 
+*/
+pair<ll, string> solve(string s, ll n){
+
+}
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(0): cout.tie(0);
+
+    ll n;
+    string s;
+    cin >> n >> s;
+
+    auto ans = solve(s, n);
+    cut << ans.first << '\n';
+    cout << ans.second << '\n';
+    return 0;
+}
