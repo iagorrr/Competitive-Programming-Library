fa394b8e694c676e38c86201990203c63b21d5a9
iagorrr04 <iagorrr04@gmail.com>
2022-09-25 10:07:38 -0300

    :white_check_mark: short macro added

---
+++ short_macro.cpp
@@ -0,0 +1,12 @@
+// iagorrr ;)
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio = ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0):
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+
+int main(){ 
+
+    return 0;
+}
