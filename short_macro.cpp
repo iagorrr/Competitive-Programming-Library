7744067e9f2a30fac4dd317e056bfa17a4af296f
iagorrr04 <iagorrr04@gmail.com>
2022-09-30 11:13:57 -0300

    :bug: line 4 fixed

--- short_macro.cpp
+++ short_macro.cpp
@@ -1,7 +1,7 @@
 // iagorrr ;)
 #include <bits/stdc++.h>
 using namespace std;
-#define fastio = ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0):
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 using ll = long long;
 using vll = vector<ll>;
 using pll = pair<ll, ll>;
