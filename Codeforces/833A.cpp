e04e23e058f9a28ca82602e29b14bda0d0eb7ad5
iagorrr04 <iagorrr04@gmail.com>
2022-11-13 20:58:03 -0300

    :white_check_mark: implementation, math, geometry

---
+++ Codeforces/833A.cpp
@@ -0,0 +1,22 @@
+// iagorrr ;)
+#include <bits/stdc++.h>
+#define sws ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+
+int32_t main(){
+    sws
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        cout << n/2 + n%2 << '\n';
+    }
+    return 0;
+}
+
+// AC.
