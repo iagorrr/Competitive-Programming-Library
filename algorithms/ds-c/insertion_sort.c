16faf340d23e79c8a350ea093df83f05f9a85b24
iagorrr04 <iagorrr04@gmail.com>
2023-01-28 17:36:06 -0300

    :white_check_mark: insertion sort reviewed.

--- algorithms/ds-c/insertion_sort.c
+++ algorithms/ds-c/insertion_sort.c
@@ -1,23 +1,26 @@
 // iagorrr ;)
 typedef int Item;
 
-#define LESS(a, b) a < b
-#define LESSEQ(a, b) a <= b
-#define SWAP(a , b) {Item t = a; a = b; b = t;}
+#define less(a, b) a < b
+#define lesseq(a, b) a <= b
+#define exchange(a , b) {Item t = a; a = b; b = t;}
+#define lessexchange(a, b) {if(less(a, b) exchange(a, b))}
 
+void insertion_sort(Item *v, int l, int r){// [l , r]
+    for(int i = r; i > l; --i)
+        if(v[i] > v[i-1]) exchange(v[i], v[i-1]);
 
-void insertion_sort(Item *v, int l, int r){// [l , r[
-    for(int i = r-1; i > l; --i)
-        if(v[i] > v[i-1]) SWAP(v[i], v[i-1]);
-
-    for(int i = l+1; i < r; ++i){
+    for(int i = l+1; i <= r; ++i){
         int j = i;
-        int t = v[j];
+        int pivot = v[j];
     
-        while(t <= v[j-1]){
+        while(less(pivot, v[j-1])){
             v[j] = v[j-1];
             --j;
         }
-        v[j] = t;
+
+        v[j] = pivot;
     }
 }
+
+int main(){ return 0;}
