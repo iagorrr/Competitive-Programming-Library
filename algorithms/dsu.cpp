b6267986966a441b89beedb6f27335fe27734fe9
iagorrr04 <iagorrr04@gmail.com>
2023-02-05 13:52:05 -0300

    :ambulance: find set.

--- algorithms/dsu.cpp
+++ algorithms/dsu.cpp
@@ -10,17 +10,12 @@ class DSU{
         DSU(int N) : ps(N+1), size(N+1, 1){
             iota(ps.begin(), ps.end(), 0);
         }
-        
-        // to find wich set x belongs.
         int find_set(int x){
-            return ps[x] == x ? x : ps[x] = find_set(x);
+            return ps[x] == x ? x : ps[x] = find_set(ps[x]);
         }
         bool same_set(int x, int y){
             return find_set(x) == find_set(y);
         }
-        
-        
-        // join two distinct sets.
         void union_set(int x, int y){
             if(same_set(x, y))
                 return;
@@ -28,7 +23,6 @@ class DSU{
             int px = find_set(x);
             int py = find_set(y);
 
-            // let the bigger one be px.
             if(size[px] < size[py]) swap(px, py);
 
             ps[py] = px;
