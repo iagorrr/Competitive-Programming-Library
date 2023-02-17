e24b50943bf3a622917d32333936e0375bbbffab
iagorrr04 <iagorrr04@gmail.com>
2023-02-17 19:52:20 -0300

    :white_check_mark: implementation.

---
+++ maratonasDF_unballoon/impa_ou_pa.cpp
@@ -0,0 +1,20 @@
+// iagorrr ;)
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+#define minpq  priority_queue<ll, vll, greater<ll>()>
+//•¨•.¸¸♪main•¨•.¸¸♪(　-ω-)ノ　(　・ω・)
+int32_t main(void){ fastio;
+  int a, b; cin >> a >> b;
+  a += b;
+
+  if((a&1)) cout << "Lucas\n";
+  else cout << "Pedro\n";
+
+}
+
+// AC, implementation.
+
