a05d04a390cab983e72f6aae69e116e837b02895
iagorrr04 <iagorrr04@gmail.com>
2023-02-20 12:49:15 -0300

    :white_check_mark: implementation.

---
+++ atCoder/abc063_a.cpp
@@ -0,0 +1,19 @@
+// iagorrr ;)
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+
+int main(){ 
+    fastio
+    ll a, b;
+    cin >> a >> b;
+
+    if(a+b >= 10) cout << "error\n";
+    else cout << a+b << '\n';
+    return 0;
+}
+
+// AC.
