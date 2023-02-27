65c6fcf91c7a877489cf3a51f19d4f677ccc5ef0
iagorrr04 <iagorrr04@gmail.com>
2023-02-27 11:54:47 -0300

    :construction:

--- short_macro.cpp
+++ short_macro.cpp
@@ -4,8 +4,8 @@ using namespace std;
 #define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 using ll = long long;
 using vll = vector<ll>;
+using vi = vector<int>;
 using pll = pair<ll, ll>;
-#define minpq  priority_queue<ll, vll, greater<ll>()>
-//•¨•.¸¸♪main•¨•.¸¸♪(　-ω-)ノ　(　・ω・)
+
 int32_t main(void){ fastio;
 }
