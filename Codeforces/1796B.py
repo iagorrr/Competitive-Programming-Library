ed7b73f5101195f46a140bf3e7f4c9c35547e8f4
iagorrr04 <iagorrr04@gmail.com>
2023-02-28 13:35:37 -0300

    :white_check_mark: string, numbertheory.

---
+++ Codeforces/1796B.py
@@ -0,0 +1,14 @@
+# iagorrr ;)
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    s = input()
+
+    s2 = "FBFFBFFBFBFFBFFBFBFFBFFBFBFFBFFB"
+    if(s in s2):
+        print("YES")
+    else:
+        print("NO")
+# AC, string matching, numbertheory
+
