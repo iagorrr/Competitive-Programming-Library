5b387ddbbb7bb7c9734de0a97277c5c6fb654afc
iagorrr04 <iagorrr04@gmail.com>
2023-02-28 10:01:21 -0300

    :construction:

--- short_macro.cpp
+++ short_macro.cpp
@@ -4,8 +4,11 @@ using namespace std;
 #define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 using ll = long long;
 using vll = vector<ll>;
-using vi = vector<int>;
 using pll = pair<ll, ll>;
+using vi = vector<int>;
+using pii = pair<int, int>;
+#define INV(a) for(auto &x : a) cin >> x
+#define all(a) a.begin(), a.end()
 
 int32_t main(void){ fastio;
 }
