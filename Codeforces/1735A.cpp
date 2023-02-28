5eba88c5331663137f7e0dd3061af8b6d61cfd72
iagorrr04 <iagorrr04@gmail.com>
2023-02-28 20:40:17 -0300

    :white_check_mark: math.

---
+++ Codeforces/1735A.cpp
@@ -0,0 +1,23 @@
+// iagorrr ;)
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using pii = pair<int, int>;
+#define INV(a) for(auto &x : a) cin >> x
+#define all(a) a.begin(), a.end()
+
+int32_t main(void){ fastio;
+  int t; cin >> t;
+  while(t--){
+    int n; cin >> n;
+
+    cout << (n-3)/3 - 1<< '\n';
+  
+  }
+}
+
+// AC.
