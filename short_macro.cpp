1e859f7d224227e23686ec1fa1f563496d6d3bdd
iagorrr04 <iagorrr04@gmail.com>
2023-03-04 17:00:20 -0300

    :sparkles: macro template added.

--- short_macro.cpp
+++ short_macro.cpp
@@ -10,5 +10,12 @@ using pii = pair<int, int>;
 #define INV(a) for(auto &x : a) cin >> x
 #define all(a) a.begin(), a.end()
 
+void run(){
+
+}
 int32_t main(void){ fastio;
+  int t; t = 1;
+  
+  while(t--)
+    run();
 }
