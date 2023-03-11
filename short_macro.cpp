cd07c54155beea64f61ad70150076d98a2428f79
iagorrr04 <iagorrr04@gmail.com>
2023-03-11 11:37:59 -0300

    :sparkles: debug macro added.

--- short_macro.cpp
+++ short_macro.cpp
@@ -10,6 +10,11 @@ using pii = pair<int, int>;
 #define INV(a) for(auto &x : a) cin >> x
 #define all(a) a.begin(), a.end()
 
+void dbg_out() { cerr << endl; }
+template <typename H, typename... T>
+void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
+#define dbg(...) { cerr << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__); }
+
 void run(){
 
 }
