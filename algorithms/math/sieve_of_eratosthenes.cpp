4063f4090f89c6749d3d8352ef7d06a99505861b
iagorrr04 <iagorrr04@gmail.com>
2023-05-13 21:06:13 -0300

    :bug: fix algorithm.

--- algorithms/math/sieve_of_eratosthenes.cpp
+++ algorithms/math/sieve_of_eratosthenes.cpp
@@ -4,11 +4,12 @@ using ll = long long;
 
 
 ll maxv = 1e5+1;
-vector<char> isprime(maxv, true);
+vector<char> isprime(maxv+1, true);
 void sieve(){
   isprime[0] = isprime[1] = false;
-  for(ll i = 2; 2 <= maxv; ++i) {
-    for(ll j = i*i; j <= maxv; ++j)
-      isprime[j] = false;
+  for(ll i = 2; i <= maxv; ++i) {
+    if(isprime[i])
+      for(ll j = i*i; j <= maxv; j += i)
+        isprime[j] = false;
   }
 }
