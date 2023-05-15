7757fbe460944e3066a1b92cae5c677ff920c9f5
iagorrr04 <iagorrr04@gmail.com>
2023-05-14 21:06:38 -0300

    :art: compact code.

--- submissions/CSES/money_sums_bottom_up.cpp
+++ submissions/CSES/money_sums_bottom_up.cpp
@@ -17,51 +17,34 @@ template <typename H, typename... T>
 void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
 #define dbg(...) { cerr << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__); }
 
-vector<vector<char>> dp(101, vector<char>(1e5+1, 0));
-void show(int x, int y){
-	cout << "===================================================\n";
-	for(int i = 0; i <= x; ++i) {
-		for(int j = 0; j <= y; ++j) {
-			cout << (int)dp[i][j] << ' ' ;
+set<int> money_sum(const vi &xs){
+	using vc = vector<char>;
+	using vvc = vector<vc>;
+	int _m = accumulate(all(xs), 0);
+	int _n = xs.size();
+	vvc _dp(_n+1, vc(_m+1, 0));
+	set<int> _ans;
+	_dp[0][xs[0]] = 1;
+	for(int i = 1; i < _n; ++i) {
+		for(int j = 0; j <= _m; ++j) {
+			if(j == 0 or _dp[i-1][j]) {
+				_dp[i][j+xs[i]] = 1;
+				_dp[i][j] = 1;
+			}
 		}
-		cout << '\n';
 	}
-	cout << "===================================================\n";
+
+	for(int i = 0 ; i < _n; ++i)
+		for(int j = 0; j <= _m; ++j)
+			if(_dp[i][j]) _ans.insert(j);
+	return _ans;
 }
 void run(){
 	ll n; cin >> n; 
-	vll a(n); INV(a);
+	vi a(n); INV(a);
 	
-	sort(all(a));
-	dp[0][a[0]] = 1;
-	// show(n, 20);
-	for(int i = 1; i < n; ++i) {
-		for(int j = 0; j <= 1e5; ++j) {
-			if(j==0 or dp[i-1][j]){
-				/*
-					 if with one coin less i could get a[i],
-					 now i can get a[i]+j with the i-th coin
-				 */
-				dp[i][j+a[i]] = 1;
-
-				/*
-					 if i could get it with one coin less,
-					 than i can get it discarding the a[i]
-				 */
-				dp[i][j] = 1;
-			}
-		}
-		// show(n, 20);
-	}	
-
-
-	set<ll> ans;
-	for(int i = 0; i < n; ++i) {
-		for(int j = 0; j <= 1e5; ++j) {
-			if(dp[i][j]) ans.insert(j);
-		}
-	}	
-
+	
+	auto ans = money_sum(a);
 	ans.erase(0);
 	cout << ans.size() << '\n';
 	for(auto ai : ans) cout << ai << ' ';
