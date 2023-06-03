58875dcb45d67994794163c0def71bf672d107ad
iagorrr04 <iagorrr04@gmail.com>
2023-06-03 11:05:34 -0300

    :white_check_mark: implementation.

---
+++ submissions/atCoder/abc304_a.cpp
@@ -0,0 +1,45 @@
+// iagorrr ;)
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using pii = pair<int, int>;
+#define INV(xxxx) for(auto &xxx : xxxx) cin >> xxx;
+#define all(a) a.begin(), a.end()
+#define snd second
+#define fst first
+
+void dbg_out() { cerr << endl; }
+template <typename H, typename... T>
+void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
+#define dbg(...) { cerr << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__); }
+
+void run(){
+  ll n; cin >> n;
+  vector<pair<string, int>> xs(n);
+  for(auto &[si, ai] : xs) {
+    cin >> si >> ai;
+  }
+  int ypos = 0;
+  for(int i = 0; i < n; ++i) {
+    if(xs[i].second < xs[ypos].second) ypos = i;
+  }
+
+
+  int cnt = 0;
+  while(cnt < n) {
+    cout << xs[(ypos+cnt)%n].first << '\n';
+    cnt++;
+  }
+}
+int32_t main(void){ fastio;
+  int t; t = 1;
+  
+  while(t--)
+    run();
+}
+
+// AC, implementation
