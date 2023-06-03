1964d3e3891e1d37a37588d51d536bced4b87ff0
iagorrr04 <iagorrr04@gmail.com>
2023-06-03 19:33:22 -0300

    :white_check_mark: math, ad-hoc.

---
+++ submissions/atCoder/abc304_b.cpp
@@ -0,0 +1,35 @@
+// iagorrr ;)
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using pii = pair<int, int>;
+#define INV(xxxx) for(auto &xxx : xxxx) cin >> xxx;
+#define all(a) a.begin(), a.end()
+#define snd second
+#define fst first
+
+void dbg_out() { cerr << endl; }
+template <typename H, typename... T>
+void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
+#define dbg(...) { cerr << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__); }
+
+void run(){
+  string s; cin >> s;
+  int totruc = s.size()-3;
+  for(int i = 0; i < totruc; ++i){
+    s[s.size()-1-i] = '0';
+  }
+  cout << s << '\n';
+}
+int32_t main(void){ fastio;
+  int t; t = 1;
+  
+  while(t--)
+    run();
+}
+
+// AC, math, implementation
