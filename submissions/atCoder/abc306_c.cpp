9c459751c37296761b807b903f93bddce714a64f
iagorrr04 <iagorrr04@gmail.com>
2023-06-17 23:38:33 -0300

    :white_check_mark: implementation.

---
+++ submissions/atCoder/abc306_c.cpp
@@ -0,0 +1,59 @@
+// iagorrr ;)
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                                                                 \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(0);                                                                  \
+  cout.tie(0);
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using pii = pair<int, int>;
+#define INV(xxxx)                                                              \
+  for (auto &xxx : xxxx)                                                       \
+    cin >> xxx;
+#define all(a) a.begin(), a.end()
+#define snd second
+#define fst first
+
+void dbg_out() { cerr << endl; }
+template <typename H, typename... T> void dbg_out(H h, T... t) {
+  cerr << ' ' << h;
+  dbg_out(t...);
+}
+#define dbg(...)                                                               \
+  {                                                                            \
+    cerr << #__VA_ARGS__ << ':';                                               \
+    dbg_out(__VA_ARGS__);                                                      \
+  }
+
+void run() {
+  ll n;
+  cin >> n;
+  vector<pair<ll, ll>> b;
+  multiset<ll> app;
+  for (int i = 0; i < 3 * n; ++i) {
+    ll x;
+    cin >> x;
+    app.insert(x);
+    if (app.count(x) == 2) {
+      b.push_back({i, x});
+    }
+  }
+
+  for (auto [i, x] : b) {
+    cout << x << ' ';
+  }
+  cout << '\n';
+}
+int32_t main(void) {
+  fastio;
+  int t;
+  t = 1;
+
+  while (t--)
+    run();
+}
+
+// AC, implementation
