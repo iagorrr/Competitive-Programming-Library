1ea3d0bc951546103ebe0da139fba04cd30b0f3f
iagorrr04 <iagorrr04@gmail.com>
2023-06-20 21:21:14 -0300

    :white_check_mark: tree, math.

---
+++ submissions/Codeforces/1836C.cpp
@@ -0,0 +1,50 @@
+// iagorrr ;)
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                                                                 \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(0);                                                                  \
+  cout.tie(0);
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using pii = pair<int, int>;
+#define INV(xxxx)                                                              \
+  for (auto &xxx : xxxx)                                                       \
+    cin >> xxx;
+#define all(a) a.begin(), a.end()
+#define snd second
+#define fst first
+
+void dbg_out() { cerr << endl; }
+template <typename H, typename... T> void dbg_out(H h, T... t) {
+  cerr << ' ' << h;
+  dbg_out(t...);
+}
+#define dbg(...)                                                               \
+  {                                                                            \
+    cerr << #__VA_ARGS__ << ':';                                               \
+    dbg_out(__VA_ARGS__);                                                      \
+  }
+
+void run() {
+  ll n;
+  cin >> n;
+  ll ans = 0;
+  while (n >= 1) {
+    ans += n;
+    n /= 2;
+  }
+  cout << ans << '\n';
+}
+int32_t main(void) {
+  fastio;
+  int t;
+  t = 1;
+  cin >> t;
+  while (t--)
+    run();
+}
+
+// AC, trees, math
