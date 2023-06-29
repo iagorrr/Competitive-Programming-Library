8e47a68ad91161c423b633735c213f6919c43556
iagorrr04 <iagorrr04@gmail.com>
2023-06-29 17:48:52 -0300

    :white_check_mark: strings

---
+++ submissions/CSES/creating_strings.cpp
@@ -0,0 +1,53 @@
+// iagorrr ;)
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                                                                 \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(0);                                                                  \
+  cout.tie(0);
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using pii = pair<int, int>;
+#define INV(xxxx)                                                              \
+  for (auto &xxx : xxxx)                                                       \
+    cin >> xxx;
+#define all(a) a.begin(), a.end()
+#define snd second
+#define fst first
+
+void dbg_out() { cerr << endl; }
+template <typename H, typename... T> void dbg_out(H h, T... t) {
+  cerr << ' ' << h;
+  dbg_out(t...);
+}
+#define dbg(...)                                                               \
+  {                                                                            \
+    cerr << #__VA_ARGS__ << ':';                                               \
+    dbg_out(__VA_ARGS__);                                                      \
+  }
+
+void run() {
+  string a;
+  cin >> a;
+  vector<string> ans;
+  sort(all(a));
+  do {
+    ans.push_back(a);
+  } while (next_permutation(all(a)));
+  cout << ans.size() << '\n';
+  for(auto xi : ans) {
+    cout << xi << '\n';
+  }
+}
+int32_t main(void) {
+  fastio;
+  int t;
+  t = 1;
+
+  while (t--)
+    run();
+}
+// AC, permutation
