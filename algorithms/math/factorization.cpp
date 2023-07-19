67706980b172b503d31368011c3c8176d903c8ec
iagorrr04 <iagorrr04@gmail.com>
2023-07-19 10:20:31 -0300

    :art: adds complexity and right formatation

--- algorithms/math/factorization.cpp
+++ algorithms/math/factorization.cpp
@@ -1,12 +1,13 @@
+// O(sqrt(n))
 map<ll, ll> factorization(ll n)
 {
         map<ll, ll> ans;
         for (ll i = 2; i*i <= n; i++)
         {
-            ll count = 0;
-            for (; n%i==0; count++, n/=i);
-            if (count)
-                      ans[i] = count;
+                ll count = 0;
+                for (; n%i==0; count++, n/=i);
+                if (count)
+                        ans[i] = count;
         }
         if (n > 1) ans[n]++;
         return ans;
