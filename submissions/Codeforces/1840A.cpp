8b9e86f164b0678837c850080a93b6bf21603fcb
iagorrr04 <iagorrr04@gmail.com>
2023-07-24 21:02:09 -0300

    :white_check_mark: strings, ad-hoc.

---
+++ submissions/Codeforces/1840A.cpp
@@ -0,0 +1,58 @@
+// iagorrr ;)
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define fastio                            \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(0);                       \
+        cout.tie(0);
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using pii = pair<int, int>;
+#define INV(xxxx) \
+        for (auto &xxx : xxxx) cin >> xxx;
+#define all(a) a.begin(), a.end()
+#define snd second
+#define fst first
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+
+void dbg_out() { cerr << endl; }
+template <typename H, typename... T>
+void dbg_out(H h, T... t) {
+        cerr << ' ' << h;
+        dbg_out(t...);
+}
+#define dbg(...)                             \
+        {                                    \
+                cerr << #__VA_ARGS__ << ':'; \
+                dbg_out(__VA_ARGS__);        \
+        }
+
+void run() {
+        ll n;
+        cin >> n;
+        string s;
+        cin >> s;
+        for (int i = 0; i < n; ++i) {
+                cout << s[i];
+                int j = i;
+                do {
+                        i++;
+                } while (s[j] != s[i]);
+        }
+        cout << '\n';
+}
+int32_t main(void) {
+        // fastio;
+        int t;
+        t = 1;
+        cin >> t;
+        while (t--) run();
+}
+
+// AC, ad-hoc
