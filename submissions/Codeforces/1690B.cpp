e990696f6627cd2765fb557c8f9448857948db7c
iagorrr04 <iagorrr04@gmail.com>
2023-08-02 16:50:56 -0300

    :white_check_mark: greedy, implementation

---
+++ submissions/Codeforces/1690B.cpp
@@ -0,0 +1,70 @@
+// iagorrr ;)
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define fastio                            \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(0);                       \
+        cout.tie(0);
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using pii = pair<int, int>;
+#define INV(xxxx) \
+        for (auto &xxx : xxxx) cin >> xxx;
+#define all(a) a.begin(), a.end()
+#define snd second
+#define fst first
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+
+void dbg_out() { cerr << endl; }
+template <typename H, typename... T>
+void dbg_out(H h, T... t) {
+        cerr << ' ' << h;
+        dbg_out(t...);
+}
+#define dbg(...)                             \
+        {                                    \
+                cerr << #__VA_ARGS__ << ':'; \
+                dbg_out(__VA_ARGS__);        \
+        }
+const ll INF = 1e18;
+
+void run() {
+        ll n;
+        cin >> n;
+        vll a(n);
+        INV(a);
+        vll b(n);
+        INV(b);
+
+        vll c(n);
+        ll maxc = 0;
+        for (int i = 0; i < n; ++i) {
+                c[i] = a[i] - b[i];
+                if (c[i] < 0) {
+                        cout << "No\n";
+                        return;
+                }
+                maxc = max(maxc, c[i]);
+        }
+
+        for (int i = 0; i < n; ++i) {
+                if (max(0ll, a[i] - maxc) != b[i]) {
+                        cout << "No\n";
+                        return;
+                }
+        }
+        cout << "Yes\n";
+}
+int32_t main(void) {
+        fastio;
+        int t;
+        t = 1;
+        cin >> t;
+        while (t--) run();
+}
