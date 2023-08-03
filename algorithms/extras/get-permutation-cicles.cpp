3a6a2e2e530076be18ebbd0a60a4a9185db5bf86
iagorrr04 <iagorrr04@gmail.com>
2023-08-02 22:45:44 -0300

    :sparkles: adds algorithm to find the cicles of a permutation

---
+++ algorithms/extras/get-permutation-cicles.cpp
@@ -0,0 +1,24 @@
+/*
+ * receives a permutation [0, n-1]
+ * returns a vector of cicles
+ * for example: [ 1, 0, 3, 4, 2] -> [[0, 1], [2, 3, 4]]
+ * */
+vector<vll> getPermutationCicles(const vll &ps) {
+        ll n = len(ps);
+        vector<char> visited(n);
+        vector<vll> cicles;
+        for (int i = 0; i < n; ++i) {
+                if (visited[i]) continue;
+
+                vll cicle;
+                ll pos = i;
+                while (!visited[pos]) {
+                        cicle.pb(pos);
+                        visited[pos] = true;
+                        pos = ps[pos];
+                }
+
+                cicles.push_back(vll(all(cicle)));
+        }
+        return cicles;
+}
