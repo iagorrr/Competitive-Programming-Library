0c33fe95aaebd9d2cd6a6ec8a45fedfadf313de5
iagorrr04 <iagorrr04@gmail.com>
2023-08-07 16:10:03 -0300

    :white_check_mark: math, ad-hoc

---
+++ submissions/atCoder/abc313_a.cpp
@@ -0,0 +1,67 @@
+// iagorrr ;)
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define fastio                            \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(0);                       \
+        cout.tie(0);
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define len(__x) (ll) __x.size()
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define INV(xxxx) \
+        for (auto &xxx : xxxx) cin >> xxx;
+#define PRINTV(___x)                                                  \
+        for_each(all(___x), [](ll &____x) { cout << ____x << ' '; }), \
+                cout << '\n';
+#define snd second
+#define fst first
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+#define rsz(___x, ___n) resize(___x, ___n)
+
+void dbg_out() { cerr << endl; }
+template <typename H, typename... T>
+void dbg_out(H h, T... t) {
+        cerr << ' ' << h;
+        dbg_out(t...);
+}
+#define dbg(...)                             \
+        {                                    \
+                cerr << #__VA_ARGS__ << ':'; \
+                dbg_out(__VA_ARGS__);        \
+        }
+const ll INF = 1e18;
+
+void run() {
+        ll n;
+        cin >> n;
+        ll xi;
+        cin >> xi;
+        ll mxi = 0;
+        for (int i = 1; i < n; ++i) {
+                ll x;
+                cin >> x;
+                mxi = max(mxi, x);
+        }
+
+        cout << max(0ll, (mxi + 1) - xi) << '\n';
+}
+int32_t main(void) {
+        fastio;
+        int t;
+        t = 1;
+        // cin >> t;
+        while (t--) run();
+}
+
+// AC, math, ad-hoc
