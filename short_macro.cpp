28a9382148d2ee470ee9c6217100a1c58ce2fc88
iagorrr04 <iagorrr04@gmail.com>
2023-08-14 19:24:49 -0300

    :sparkles: adds debug to macro

--- short_macro.cpp
+++ short_macro.cpp
@@ -1,11 +1,16 @@
 // iagorrr ;)
 #include <bits/stdc++.h>
 using namespace std;
+#ifdef DEBUG
+#inlude "debug.cpp"
+#else
+#define dbg(...) 666
+#endif
 #define endl '\n'
-#define fastio                            \
-        ios_base::sync_with_stdio(false); \
-        cin.tie(0);                       \
-        cout.tie(0);
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
 #define rep(i, l, r) for (int i = (l); i < (r); i++)
 #define len(__x) (ll) __x.size()
 using ll = long long;
@@ -18,34 +23,23 @@ using vii = vector<pii>;
 using vc = vector<char>;
 #define all(a) a.begin(), a.end()
 #define INV(xxxx) \
-        for (auto &xxx : xxxx) cin >> xxx;
-#define PRINTV(___x)                                                  \
-        for_each(all(___x), [](ll &____x) { cout << ____x << ' '; }), \
-                cout << '\n';
+    for (auto &xxx : xxxx) cin >> xxx;
+#define PRINTV(___x) \
+    for_each(all(___x), [](ll &____x) { cout << ____x << ' '; }), cout << '\n';
 #define snd second
 #define fst first
 #define pb(___x) push_back(___x)
 #define mp(___a, ___b) make_pair(___a, ___b)
 #define eb(___x) emplace_back(___x)
+#define rsz(___x, ___n) resize(___x, ___n)
 
-void dbg_out() { cerr << endl; }
-template <typename H, typename... T>
-void dbg_out(H h, T... t) {
-        cerr << ' ' << h;
-        dbg_out(t...);
-}
-#define dbg(...)                             \
-        {                                    \
-                cerr << #__VA_ARGS__ << ':'; \
-                dbg_out(__VA_ARGS__);        \
-        }
 const ll INF = 1e18;
 
-void run() {}
+void run() { dbg("iago"); }
 int32_t main(void) {
-        fastio;
-        int t;
-        t = 1;
-        cin >> t;
-        while (t--) run();
+    fastio;
+    int t;
+    t = 1;
+    // cin >> t;
+    while (t--) run();
 }
