065991994f539767be801503aab867b6e6f42187
iagorrr04 <iagorrr04@gmail.com>
2023-08-17 11:56:24 -0300

    :white_check_mark: math, ad-hoc

---
+++ submissions/atCoder/abc156_a.cpp
@@ -0,0 +1,56 @@
+// iagorrr ;)
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef DEBUG
+#include "debug.cpp"
+#else
+#define dbg(...) 666
+#endif
+#define endl '\n'
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define len(__x) (ll) __x.size()
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define INV(xxxx) \
+    for (auto &xxx : xxxx) cin >> xxx;
+#define PRINTV(___x) \
+    for_each(all(___x), [](ll &____x) { cout << ____x << ' '; }), cout << '\n';
+#define snd second
+#define fst first
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+#define rsz(___x, ___n) resize(___x, ___n)
+
+const ll INF = 1e18;
+
+void run() {
+    ll n, r;
+    cin >> n >> r;
+
+    if (n >= 10) {
+        cout << r << endl;
+    } else {
+        cout << r + 100 * (10 - n) << endl;
+    }
+}
+int32_t main(void) {
+    fastio;
+    int t;
+    t = 1;
+    // cin >> t;
+    while (t--) run();
+}
+
+// AC, math, ad-hoc
