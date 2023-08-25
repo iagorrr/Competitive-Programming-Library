1cb71db14a3147f80ad9b36b534ea57517bb43cc
iagorrr04 <iagorrr04@gmail.com>
2023-08-25 15:50:40 -0300

    :fire: removes unused implementations

--- algorithms/data-structures/dsu.cpp
+++ algorithms/data-structures/dsu.cpp
@@ -5,14 +5,12 @@ struct DSU {
   int find_set(int x) { return ps[x] == x ? x : ps[x] = find_set(ps[x]); }
   bool same_set(int x, int y) { return find_set(x) == find_set(y); }
   void union_set(int x, int y) {
-    if (same_set(x, y))
-      return;
+    if (same_set(x, y)) return;
 
     int px = find_set(x);
     int py = find_set(y);
 
-    if (size[px] < size[py])
-      swap(px, py);
+    if (size[px] < size[py]) swap(px, py);
 
     ps[py] = px;
     size[px] += size[py];
