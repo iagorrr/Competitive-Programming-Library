8e6f4cd1d4813c768e2c6b0de17c47e4343125c9
iagorrr04 <iagorrr04@gmail.com>
2023-08-25 14:14:22 -0300

    :white_check_mark: segment tree, range sum update, range min query

--- submissions/SPOJ-sphere/RMQSQ_3.cpp
+++ submissions/SPOJ-sphere/RMQSQ_3.cpp
@@ -1,10 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef debug
+#include "debug.cpp"
+#else
+#define dbg(...) 666
+#endif
+#define endl '\n'
+#define fastio                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define len(__x) (ll) __x.size()
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define snd second
+#define fst first
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+
+const ll inf = 1e18;
+
 template <typename t = ll>
-struct SegTree {
+struct segtree {
   int n;
   t nu;
   t nq;
   vector<t> st, lazy;
-  SegTree(const vector<t> &xs)
+  segtree(const vector<t> &xs)
     : n(len(xs)),
       nu(0),
       nq(numeric_limits<t>::max()),
@@ -13,7 +44,7 @@ struct SegTree {
     for (int i = 0; i < len(xs); ++i) update(i, i, xs[i]);
   }
 
-  SegTree(int n) : n(n), st(4 * n, nu), lazy(4 * n, nu) {}
+  segtree(int n) : n(n), st(4 * n, nu), lazy(4 * n, nu) {}
 
   void update(int l, int r, ll value) { update(1, 0, n - 1, l, r, value); }
 
@@ -71,3 +102,27 @@ struct SegTree {
   int right(int p) { return (p << 1) + 1; }
   int mid(int l, int r) { return (r - l) / 2 + l; }
 };
+
+void run() {
+  int n;
+  cin >> n;
+  vi a(n);
+  for (auto &x : a) cin >> x;
+  segtree<int> st(a);
+  int q;
+  cin >> q;
+  while (q--) {
+    int l, r;
+    cin >> l >> r;
+    cout << st.query(l, r) << endl;
+  }
+}
+int32_t main(void) {
+  fastio;
+  int t;
+  t = 1;
+  // cin >> t;
+  while (t--) run();
+}
+
+// AC, segment tree, range sum update, range min query
