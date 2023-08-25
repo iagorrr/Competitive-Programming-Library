82996f6e436d5b74e134d53574313811b31e5b85
iagorrr04 <iagorrr04@gmail.com>
2023-08-25 16:59:06 -0300

    :technologist: improves rabin-karp algorithm
    Co-authored-by: Thalisson-Alves <thalisson2030@gmail.com>

--- algorithms/strings/rabin-karp.cpp
+++ algorithms/strings/rabin-karp.cpp
@@ -1,25 +1,43 @@
-vi rabin_karp(string const &s, string const &t) {
-        ll p = 31;
-        ll m = 1e9 + 9;
-        int S = s.size(), T = t.size();
+size_t rabin_karp(const string &s, const string &p)
+{
+  if (s.size() < p.size())
+    return 0;
 
-        vll p_pow(max(S, T));
-        p_pow[0] = 1;
-        for (int i = 1; i < (int)p_pow.size(); i++)
-                p_pow[i] = (p_pow[i - 1] * p) % m;
+  auto n = s.size(), m = p.size();
+  const ll p1 = 31, p2 = 29, q1 = 1e9 + 7, q2 = 1e9 + 9;
+  const ll p1_1 = fpow(p1, q1 - 2, q1), p1_2 = fpow(p1, m - 1, q1);
+  const ll p2_1 = fpow(p2, q2 - 2, q2), p2_2 = fpow(p2, m - 1, q2);
 
-        vll h(T + 1, 0);
-        for (int i = 0; i < T; i++)
-                h[i + 1] = (h[i] + (t[i] - 'a' + 1) * p_pow[i]) % m;
-        ll h_s = 0;
-        for (int i = 0; i < S; i++)
-                h_s = (h_s + (s[i] - 'a' + 1) * p_pow[i]) % m;
+  pair<ll, ll> hs, hp;
+  for (int i = (int)m - 1; ~i; --i)
+  {
+    hs.first = (hs.first * p1) % q1;
+    hs.first = (hs.first + (s[i] - 'a' + 1)) % q1;
+    hs.second = (hs.second * p2) % q2;
+    hs.second = (hs.second + (s[i] - 'a' + 1)) % q2;
 
-        vi occurences;
-        for (int i = 0; i + S - 1 < T; i++) {
-                ll cur_h = (h[i + S] + m - h[i]) % m;
-                // IT DON'T CONSIDERE CONLISIONS !
-                if (cur_h == h_s * p_pow[i] % m) occurences.push_back(i);
-        }
-        return occurences;
+    hp.first = (hp.first * p1) % q1;
+    hp.first = (hp.first + (p[i] - 'a' + 1)) % q1;
+    hp.second = (hp.second * p2) % q2;
+    hp.second = (hp.second + (p[i] - 'a' + 1)) % q2;
+  }
+
+  size_t occ = 0;
+  for (size_t i = 0; i < n - m; i++)
+  {
+    occ += (hs == hp);
+
+    int fi = s[i] - 'a' + 1;
+    int fm = s[i + m] - 'a' + 1;
+
+    hs.first = (hs.first - fi + q1) % q1;
+    hs.first = (hs.first * p1_1) % q1;
+    hs.first = (hs.first + fm * p1_2) % q1;
+    hs.second = (hs.second - fi + q2) % q2;
+    hs.second = (hs.second * p2_1) % q2;
+    hs.second = (hs.second + fm * p2_2) % q2;
+  }
+  occ += hs == hp;
+
+  return occ;
 }
