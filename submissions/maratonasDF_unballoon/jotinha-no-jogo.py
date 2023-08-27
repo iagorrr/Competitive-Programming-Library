7a3cc976e24d3ddd79a9712832c1943eda8f6788
iagorrr04 <iagorrr04@gmail.com>
2023-08-26 21:20:52 -0300

    :sparkles: math, expected value, modular arithmetic

--- submissions/maratonasDF_unballoon/jotinha-no-jogo.py
+++ submissions/maratonasDF_unballoon/jotinha-no-jogo.py
@@ -3,3 +3,5 @@ for _ in range(int(input())):
     a, b, c = map(int, input().split())
     ans = (a - a * pow(a + b, mod - 2, mod) * c) % mod
     print(ans)
+
+# Ac, math, expected value, modular arithmetic
