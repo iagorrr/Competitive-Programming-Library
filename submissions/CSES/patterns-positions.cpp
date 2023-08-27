b6fdbaae1d7c6197fada247e197c31317b89cbcb
iagorrr04 <iagorrr04@gmail.com>
2023-08-27 16:29:25 -0300

    :sparkles: strings, first occurence, suffix automaton

--- submissions/CSES/patterns-positions.cpp
+++ submissions/CSES/patterns-positions.cpp
@@ -1,5 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define fastio                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define len(__x) (ll) __x.size()
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define snd second
+#define fst first
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+
+const ll INF = 1e18;
+
 struct state {
-  int len, link, cnt;
+  int len, link, cnt, firstpos;
   // this can be optimized using a vector with the alphabet size
   map<char, int> next;
 };
@@ -41,6 +67,7 @@ struct SuffixAutomaton {
   void add_char(char c) {
     int cur = sz++;
     st[cur].len = st[last].len + 1;
+    st[cur].firstpos = st[cur].len - 1;
     int p = last;
     // follow the suffix link until find a transition to c
     while (p != -1 and !st[p].next.count(c)) {
@@ -63,6 +90,7 @@ struct SuffixAutomaton {
       st[clone].len = st[p].len + 1;
       st[clone].next = st[q].next;
       st[clone].link = st[q].link;
+      st[clone].firstpos = st[q].firstpos;
       while (p != -1 and st[p].next[c] == q) {
         st[p].next[c] = clone;
         p = st[p].link;
@@ -97,4 +125,36 @@ struct SuffixAutomaton {
     }
     return st[cur].cnt;
   }
+
+  // find the first index where t appears a substring O(len(t))
+  ll firstOccurence(const string &t) {
+    int cur = 0;
+    for (auto c : t) {
+      if (!st[cur].next.count(c)) return -1;
+      cur = st[cur].next[c];
+    }
+    return st[cur].firstpos - len(t) + 1;
+  }
 };
+void run() {
+  string s;
+  cin >> s;
+  SuffixAutomaton sa(s, len(s));
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    string t;
+    cin >> t;
+    auto fo = sa.firstOccurence(t);
+    cout << (fo == -1 ? -1 : fo+1) << endl;
+  }
+}
+int32_t main(void) {
+  fastio;
+  int t;
+  t = 1;
+  // cin >> t;
+  while (t--) run();
+}
+
+// AC, strings, first occurence, suffix automaton
