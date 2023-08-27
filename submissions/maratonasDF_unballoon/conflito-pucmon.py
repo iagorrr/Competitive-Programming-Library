66d7365c4ad63bf77d5a8c49177d09a46846621b
iagorrr04 <iagorrr04@gmail.com>
2023-08-26 21:16:45 -0300

    :sparkles: ad hoc

---
+++ submissions/maratonasDF_unballoon/conflito-pucmon.py
@@ -0,0 +1,16 @@
+a, b = map(str, input().split())
+c = a
+if b == "fogo":
+    c += "zard"
+if b == "eletricidade":
+    c += "chu"
+if b == "agua":
+    c += "tle"
+
+if b == "grama":
+    c += "ssauro"
+
+if b == "lutador":
+    c += "pablo"
+
+print(c)
