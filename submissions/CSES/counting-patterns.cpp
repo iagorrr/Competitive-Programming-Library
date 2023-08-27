2d8589390a3eecfbf5499ff0a788068ac964846e
iagorrr04 <iagorrr04@gmail.com>
2023-08-27 15:56:45 -0300

    :white_check_mark: string, count occurences, suffix automaton

---
+++ submissions/CSES/counting-patterns.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define fastio                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define len(__x) (ll) __x.size()
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define snd second
+#define fst first
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+
+const ll INF = 1e18;
+
+struct state {
+  int len, link, cnt;
+  // this can be optimized using a vector with the alphabet size
+  map<char, int> next;
+};
+
+struct SuffixAutomaton {
+  vector<state> st;
+  int sz = 0;
+  int last;
+  vc cloned;
+
+  SuffixAutomaton(const string &s, int maxlen)
+    : st(maxlen * 2), cloned(maxlen * 2) {
+    st[0].len = 0;
+    st[0].link = -1;
+    sz++;
+    last = 0;
+    for (auto &c : s) add_char(c);
+
+    // precompute cnt
+    for (int i = 1; i < sz; i++) {
+      st[i].cnt = !cloned[i];
+    }
+    vector<pair<state, int>> aux;
+    for (int i = 0; i < sz; i++) {
+      aux.push_back({st[i], i});
+    }
+
+    sort(all(aux), [](const pair<state, int> &a, const pair<state, int> &b) {
+      return a.fst.len > b.fst.len;
+    });
+
+    for (auto &[s, id] : aux) {
+      if (s.link != -1) {
+        st[s.link].cnt += st[id].cnt;
+      }
+    }
+  }
+
+  void add_char(char c) {
+    int cur = sz++;
+    st[cur].len = st[last].len + 1;
+    int p = last;
+    // follow the suffix link until find a transition to c
+    while (p != -1 and !st[p].next.count(c)) {
+      st[p].next[c] = cur;
+      p = st[p].link;
+    }
+    // there was no transition to c so create and leave
+    if (p == -1) {
+      st[cur].link = 0;
+      last = cur;
+      return;
+    }
+
+    int q = st[p].next[c];
+    if (st[p].len + 1 == st[q].len) {
+      st[cur].link = q;
+    } else {
+      int clone = sz++;
+      cloned[clone] = true;
+      st[clone].len = st[p].len + 1;
+      st[clone].next = st[q].next;
+      st[clone].link = st[q].link;
+      while (p != -1 and st[p].next[c] == q) {
+        st[p].next[c] = clone;
+        p = st[p].link;
+      }
+      st[q].link = st[cur].link = clone;
+    }
+    last = cur;
+  }
+
+  bool checkOccurrence(const string &t) {  // O(len(t))
+    int cur = 0;
+    for (auto &c : t) {
+      if (!st[cur].next.count(c)) return false;
+      cur = st[cur].next[c];
+    }
+    return true;
+  }
+  ll totalSubstrings() {  // distinct, O(len(s))
+    ll tot = 0;
+    for (int i = 1; i < sz; i++) {
+      tot += st[i].len - st[st[i].link].len;
+    }
+    return tot;
+  }
+
+  // count occurences of a given string t
+  ll countOccurences(const string &t) {
+    int cur = 0;
+    for (auto &c : t) {
+      if (!st[cur].next.count(c)) return 0;
+      cur = st[cur].next[c];
+    }
+    return st[cur].cnt;
+  }
+};
+void run() {
+  string s;
+  cin >> s;
+  SuffixAutomaton sa(s, len(s));
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    string t;
+    cin >> t;
+    cout << sa.countOccurences(t) << endl;
+  }
+}
+int32_t main(void) {
+  fastio;
+  int t;
+  t = 1;
+  // cin >> t;
+  while (t--) run();
+}
+// AC, string, count occurences, suffix automaton
+
