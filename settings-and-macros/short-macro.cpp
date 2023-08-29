51f66a22088803820346ef3a4f520115121d3083
iagorrr04 <iagorrr04@gmail.com>
2023-08-29 07:43:09 -0300

    :technologist: adds a short macro

---
+++ settings-and-macros/short-macro.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define fastio                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define len(__x) (int) __x.size()
+using ll = long long;
+using pii = pair<int, int>;
+#define all(a) a.begin(), a.end()
+
+
+void run() {}
+int32_t main(void) {
+  fastio;
+  int t;
+  t = 1;
+  // cin >> t;
+  while (t--) run();
+}
