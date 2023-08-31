88ae4ad07bb67b882b7366cd1cf546d494d6795c
iagorrr04 <iagorrr04@gmail.com>
2023-08-31 10:46:53 -0300

    :technologist: improves ordered set explanation and implementation

--- algorithms/data-structures/ordered_set_gnu_pbds.cpp
+++ algorithms/data-structures/ordered_set_gnu_pbds.cpp
@@ -1,10 +1,7 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-template <typename T>
-// using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,
-// tree_order_statistics_node_update>;
 
-// if you want to find the elements less or equal :p
-using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag,
-                         tree_order_statistics_node_update>;
+template <typename T>
+using ordered_set =
+  tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
