0cc261313955a42df68a9902a6a973ba7b62ec97
iagorrr04 <iagorrr04@gmail.com>
2023-09-01 08:27:27 -0300

    :technologist: improves data structures section description

---
+++ algorithms/data-structures/segtree-pud-rqd.cpp
@@ -0,0 +1,46 @@
+#define F(expr) [](auto a, auto b) { return expr; }
+template <typename T>
+struct SegTree {
+  using Operation = T (*)(T, T);
+
+  int N;
+  vector<T> ns;
+  Operation operation;
+  T identity;
+
+  SegTree(int n, Operation op = F(a + b), T neutral = T())
+    : N(n), ns(2 * N, neutral), operation(op), identity(neutral) {}
+
+  SegTree(const vector<T> &v, Operation op = F(a + b), T neutral = T())
+    : SegTree((int)v.size(), op, neutral) {
+    copy(v.begin(), v.end(), ns.begin() + N);
+
+    for (int i = N - 1; i > 0; --i) ns[i] = operation(ns[2 * i], ns[2 * i + 1]);
+  }
+
+  T query(size_t i) const { return ns[i + N]; }
+
+  T query(size_t l, size_t r) const {
+    auto a = l + N, b = r + N;
+    auto ans = identity;
+
+    while (a <= b) {
+      if (a & 1) ans = operation(ans, ns[a++]);
+      if (not(b & 1)) ans = operation(ans, ns[b--]);
+
+      a /= 2;
+      b /= 2;
+    }
+
+    return ans;
+  }
+
+  void update(size_t i, T value) { update_set(i, operation(ns[i + N], value)); }
+
+  void update_set(size_t i, T value) {
+    auto a = i + N;
+
+    ns[a] = value;
+    while (a >>= 1) ns[a] = operation(ns[2 * a], ns[2 * a + 1]);
+  }
+};
