96aa7fc8929084c3340032dfcbee7736cb4dbd6a
iagorrr04 <iagorrr04@gmail.com>
2023-09-05 14:29:50 -0300

    :technologist: improves dijkstra implementation and description

--- algorithms/graphs/dijkstra.cpp
+++ algorithms/graphs/dijkstra.cpp
@@ -1,10 +1,9 @@
-ll __inf = LLONG_MAX >> 5;
-vll dijkstra(const vector<vector<pll>> &g, ll n) {
+vll dijkstra(const vector<vector<pll>> &g, int n, int s) {
   priority_queue<pll, vector<pll>, greater<pll>> pq;
-  vll dist(n, __inf);
-  vector<char> vis(n);
-  pq.emplace(0, 0);
-  dist[0] = 0;
+  vll dist(n + 1, oo);
+  vector<char> vis(n + 1);
+  pq.emplace(0, s);
+  dist[s] = 0;
   while (!pq.empty()) {
     auto [d1, v] = pq.top();
     pq.pop();
