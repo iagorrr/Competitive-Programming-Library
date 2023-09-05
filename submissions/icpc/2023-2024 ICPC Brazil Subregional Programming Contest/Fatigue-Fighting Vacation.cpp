ef0e4397b2cb65979bbadb8f4e80a68d085060a8
iagorrr04 <iagorrr04@gmail.com>
2023-09-05 12:11:13 -0300

    :white_check_mark: implementation

---
+++ submissions/icpc/2023-2024 ICPC Brazil Subregional Programming Contest/Fatigue-Fighting Vacation.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define fastio                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define len(__x) (int)__x.size()
+using ll = long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define snd second
+#define fst first
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+
+const ll INF = 1e18;
+
+void run() {
+  int d, c, r;
+  cin >> d >> c >> r;
+
+  vi cs(c);
+  for (auto &ci : cs) cin >> ci;
+
+  vi rs(r);
+  for (auto &ri : rs) cin >> ri;
+
+  int pc = 0, pr = 0;
+  int ans = 0;
+  while (pc < c and pr < r) {
+    if (d >= cs[pc]) {
+      d -= cs[pc];
+      pc++;
+      ans++;
+    } else {
+      d += rs[pr];
+      pr++;
+      ans++;
+    }
+  }
+
+  while (pc < c and d >= cs[pc]) {
+    d -= cs[pc];
+    pc++;
+    ans++;
+  }
+
+  if (pc == c) ans += r - pr;
+
+  cout << ans << endl;
+}
+
+int32_t main(void) {
+  fastio;
+  int t;
+  t = 1;
+  // cin >> t;
+  while (t--) run();
+}
+
+// AC, implementation
