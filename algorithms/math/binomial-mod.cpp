825a55a0079f1df47d7a13ba8e38ebed93277d09
iagorrr04 <iagorrr04@gmail.com>
2023-09-06 19:37:52 -0300

    :technologist: improves binomial mod implementation

--- algorithms/math/binomial-mod.cpp
+++ algorithms/math/binomial-mod.cpp
@@ -1,22 +1,22 @@
-const ll maxn = 1e6;
+const ll MOD = 1e9 + 7;
+const ll maxn = 2 * 1e6;
 vll fats(maxn + 1, -1);
-const ll mod = 1e9 + 7;
 void precompute() {
   fats[0] = 1;
   for (ll i = 1; i <= maxn; i++) {
-    fats[i] = (fats[i - 1] * i) % mod;
+    fats[i] = (fats[i - 1] * i) % MOD;
   }
 }
 
 ll fpow(ll a, ll n, ll mod = LLONG_MAX) {
-  if (n == 0) return 1;
-  if (n == 1) return a;
-  ll x = fpow(a, n / 2, mod) % mod;
-  return ((x * x) % mod * (n & 1 ? a : 1ll)) % mod;
+  if (n == 0ll) return 1ll;
+  if (n == 1ll) return a;
+  ll x = fpow(a, n / 2ll, mod) % mod;
+  return ((x * x) % mod * (n & 1ll ? a : 1ll)) % mod;
 }
 
 ll binommod(ll n, ll k) {
   ll upper = fats[n];
-  ll lower = (fats[k] * fats[n - k]) % mod;
-  return (upper * fpow(lower, mod - 2, mod)) % mod;
+  ll lower = (fats[k] * fats[n - k]) % MOD;
+  return (upper * fpow(lower, MOD - 2ll, MOD)) % MOD;
 }
