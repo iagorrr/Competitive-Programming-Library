7b62b1311f22145ea7f279af43b2e6429069a457
iagorrr04 <iagorrr04@gmail.com>
2023-09-06 14:18:31 -0300

    :white_check_mark: math

---
+++ submissions/CSES/counting-divisors.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define fastio                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define len(__x) (int)__x.size()
+using ll = long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define snd second
+#define fst first
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+
+const ll oo = 1e18;
+
+void run() {
+  int x;
+  cin >> x;
+
+  int ans = 0;
+  for (int i = 1; i * i <= x; i++) {
+    if (x % i) continue;
+    ans++;
+    ans += (i * i != x);
+  }
+  cout << ans << endl;
+}
+int32_t main(void) {
+  fastio;
+  int t;
+  t = 1;
+  cin >> t;
+  while (t--) run();
+}
+
+// AC, math
