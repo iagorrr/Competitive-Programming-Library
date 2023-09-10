c431b7092313b9b642a4625da23fb77f1caacc2a
iagorrr04 <iagorrr04@gmail.com>
2023-09-10 17:36:38 -0300

    :bug: fix wrong use of visited

--- algorithms/graphs/dijkstra.cpp
+++ algorithms/graphs/dijkstra.cpp
@@ -1,14 +1,12 @@
 vll dijkstra(const vector<vector<pll>> &g, int n, int s) {
   priority_queue<pll, vector<pll>, greater<pll>> pq;
   vll dist(n + 1, oo);
-  vector<char> vis(n + 1);
   pq.emplace(0, s);
   dist[s] = 0;
   while (!pq.empty()) {
     auto [d1, v] = pq.top();
     pq.pop();
-    if (vis[v]) continue;
-    vis[v] = true;
+    if (dist[v] < d1) continue;
 
     for (auto [d2, u] : g[v]) {
       if (dist[u] > d1 + d2) {
