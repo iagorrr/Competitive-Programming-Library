e7a706bdbc7a25a21bead84e691db60ea96c208c
iagorrr04 <iagorrr04@gmail.com>
2023-09-10 20:00:56 -0300

    :technologist: improves graphs section

---
+++ algorithms/graphs/dijkstra-(k-shortest-paths).cpp
@@ -0,0 +1,30 @@
+const ll oo = 1e9 * 1e5 + 1;
+using adj = vector<vector<pll>>;
+vector<priority_queue<ll>> dijkstra(const vector<vector<pll>> &g, int n, int s,
+                                    int k) {
+  priority_queue<pll, vector<pll>, greater<pll>> pq;
+
+  vector<priority_queue<ll>> dist(n);
+  dist[0].emplace(0);
+  pq.emplace(0, s);
+  while (!pq.empty()) {
+    auto [d1, v] = pq.top();
+    pq.pop();
+
+    if (not dist[v].empty() and dist[v].top() < d1) continue;
+
+    for (auto [d2, u] : g[v]) {
+      if (len(dist[u]) < k) {
+        pq.emplace(d2 + d1, u);
+        dist[u].emplace(d2 + d1);
+      } else {
+        if (dist[u].top() > d1 + d2) {
+          dist[u].pop();
+          dist[u].emplace(d1 + d2);
+          pq.emplace(d2 + d1, u);
+        }
+      }
+    }
+  }
+  return dist;
+}
