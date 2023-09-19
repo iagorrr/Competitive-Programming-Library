5433070898daf2d6a89b2f3feb4beaa8c1476b6b
iagorrr04 <iagorrr04@gmail.com>
2023-09-19 14:36:26 -0300

    :white_check_mark: binary search

--- algorithms/data-structures/dsu.cpp
+++ algorithms/data-structures/dsu.cpp
@@ -1,7 +1,11 @@
 struct DSU {
   vi ps;
   vi size;
-  DSU(int N) : ps(N + 1), size(N + 1, 1) { iota(all(ps), 0); }
+  // vector<unordered_set<int>> sts;
+  DSU(int N) : ps(N + 1), size(N, 1), sts(N) {
+    iota(all(ps), 0);
+    // for (int i = 0; i < N; i++) sts[i].insert(i);
+  }
   int find_set(int x) { return ps[x] == x ? x : ps[x] = find_set(ps[x]); }
   bool same_set(int x, int y) { return find_set(x) == find_set(y); }
   void union_set(int x, int y) {
@@ -14,5 +18,6 @@ struct DSU {
 
     ps[py] = px;
     size[px] += size[py];
+    // sts[px].merge(sts[py]);
   }
 };
