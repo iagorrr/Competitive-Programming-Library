1fdaa571c0b6f9be5e2caf3099c5f84cf80b9433
iagorrr04 <iagorrr04@gmail.com>
2023-09-21 18:53:28 -0300

    :white_check_mark: number theory

---
+++ submissions/atCoder/abc174_c.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define fastio                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define len(__x) (int)__x.size()
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define snd second
+#define fst first
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+
+vector<string> dir({"LU", "U", "RU", "R", "RD", "D", "LD", "L"});
+int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0};
+int dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+const ll oo = 1e18;
+
+void run() {
+  ll k;
+  cin >> k;
+  vc vis(1e6 + 1);
+
+  ll i = 1;
+  ll k10 = 7;
+  ll cur = 7;
+  for (;;) {
+    if (vis[cur]) break;
+    if (cur % k == 0) {
+      cout << i << endl;
+      return;
+    }
+    vis[cur] = true;
+    k10 = (k10 * 10) % k;
+    cur = (cur + k10) % k;
+    i++;
+  }
+  cout << -1 << endl;
+}
+int32_t main(void) {
+  fastio;
+  int t;
+  t = 1;
+  // cin >> t;
+  while (t--) run();
+}
+
+// AC, number theory
