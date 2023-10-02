6d200232637a0a09dd54e8b89e1abcb75d1d94b2
iagorrr04 <iagorrr04@gmail.com>
2023-10-02 12:43:46 -0300

    :white_check_mark: strings

---
+++ submissions/atCoder/abc322_a.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define fastio                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define len(__x) (int)__x.size()
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+
+vector<string> dir({"LU", "U", "RU", "R", "RD", "D", "LD", "L"});
+int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0};
+int dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+const ll oo = 1e18;
+
+auto solve() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  auto ans = s.find("ABC");
+  if (ans == string::npos) {
+    cout << -1 << endl;
+  } else {
+    cout << 1 + ans << endl;
+  }
+}
+int32_t main(void) {
+  fastio;
+  int t;
+  t = 1;
+  // cin >> t;
+  while (t--) solve();
+}
