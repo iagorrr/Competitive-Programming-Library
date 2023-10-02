9d0ef7ef265b3a35484c55711150ba073d0b2a98
iagorrr04 <iagorrr04@gmail.com>
2023-10-02 12:45:36 -0300

    :white_check_mark: implementation

---
+++ submissions/atCoder/abc322_c.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define fastio                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define len(__x) (int)__x.size()
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+
+vector<string> dir({"LU", "U", "RU", "R", "RD", "D", "LD", "L"});
+int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0};
+int dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+const ll oo = 1e18;
+
+auto solve() {
+  ll n, m;
+  cin >> n >> m;
+  vll xs(m);
+  for (auto &xi : xs) cin >> xi;
+  sort(all(xs));
+
+  int p = 0;
+  for (int i = 1; i <= n; i++) {
+    while (p < m and xs[p] < i) p++;
+    cout << xs[p] - i << endl;
+  }
+}
+int32_t main(void) {
+  fastio;
+  int t;
+  t = 1;
+  // cin >> t;
+  while (t--) solve();
+}
+
+// AC, implementation
