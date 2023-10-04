baa35f5280e79bca4eab663988a621859e952ee6
iagorrr04 <iagorrr04@gmail.com>
2023-10-04 11:42:15 -0300

    :white_check_mark: floyd warshall, dijkstra

--- submissions/ICPC/2023-2024 ICPC Brazil Subregional Programming Contest/Detour.cpp
+++ submissions/ICPC/2023-2024 ICPC Brazil Subregional Programming Contest/Detour.cpp
@@ -7,6 +7,7 @@ using namespace std;
   cout.tie(0);
 #define len(__x) (int)__x.size()
 using ll = long long;
+using ull = unsigned long long;
 using ld = long double;
 using vll = vector<ll>;
 using pll = pair<ll, ll>;
@@ -17,73 +18,93 @@ using pii = pair<int, int>;
 using vii = vector<pii>;
 using vc = vector<char>;
 #define all(a) a.begin(), a.end()
-#define snd second
-#define fst first
 #define pb(___x) push_back(___x)
 #define mp(___a, ___b) make_pair(___a, ___b)
 #define eb(___x) emplace_back(___x)
 
-const ll INF = 1000000000;
+vector<string> dir({"LU", "U", "RU", "R", "RD", "D", "LD", "L"});
+int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0};
+int dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+const int oo = 1'000'000 * 300 + 1;
+const int MAXN(300);
+int N, M;
+vector<pii> g[MAXN];
+vi2d dist1(MAXN, vi(MAXN, oo));
+vi2d dist2(MAXN, vi(MAXN, oo));
 
-vll dijkstra(vector<vector<pll>> &adj, int s, int s2) {
-  int n = len(adj);
-  vll d(n + 1, INF);
-
-  d[s] = 0;
-  set<pair<int, int>> q;
-  q.insert({0, s});
-  while (!q.empty()) {
-    int v = q.begin()->second;
-    q.erase(q.begin());
-
-    for (auto edge : adj[v]) {
-      ll to = edge.second;
-      ll len = edge.first;
-      if (v == s and to == s2) continue;
-      if (v == s2 and to == s) continue;
-      if (d[v] + len < d[to]) {
-        q.erase({d[to], to});
-        d[to] = d[v] + len;
-        q.insert({d[to], to});
+void floyd_warshall() {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      for (int k = 0; k < N; k++) {
+        dist2[j][k] = min(dist2[j][k], dist2[j][i] + dist2[i][k]);
       }
     }
   }
-  return d;
 }
-void run() {
-  int n, m;
-  cin >> n >> m;
 
-  vector<vector<pll>> g(n + 1);
-  vector<array<ll, 3>> q;
-  for (int i = 0; i < m; i++) {
-    int u, v;
-    ll l;
-    cin >> u >> v >> l;
-    g[u].emplace_back(l, v);
-    g[v].emplace_back(l, u);
+vi d(MAXN);
+int dijkstra(int s, int e) {
+  fill(all(d), oo);
+  d[s] = 0;
+  priority_queue<pii, vector<pii>, greater<pii>> pq;
+  pq.emplace(0, s);
 
-    q.push_back({u, v, l});
-  }
+  while (not pq.empty()) {
+    auto [du, u] = pq.top();
+    pq.pop();
+    if (u == e) return d[e];
+    if (du > d[u]) continue;
 
-  vll2d dists(n + 1, vll(n + 1, INF));
-  for (int i = 1; i <= n; i++) {
-    for (int j = i + 1; j <= n; j++) {
-      auto dist = dijkstra(g, i, j);
-      dists[i][j] = dist[j];
-      dists[j][i] = dist[j];
+    for (auto &[v, dv] : g[u]) {
+      if ((u == s and v == e) or (u == e and v == s)) continue;
+      if (dv + du < d[v]) {
+        d[v] = dv + du;
+        pq.emplace(d[v], v);
+      }
     }
   }
 
-  for (auto &[u, v, l] : q) {
-    if (u > v) swap(u, v);
-    cout << (dists[u][v] == INF ? -1 : dists[u][v]) << endl;
+  return d[e];
+}
+auto solve() {
+  cin >> N >> M;
+
+  vector<pii> es;
+  while (M--) {
+    int u, v, l;
+    cin >> u >> v >> l;
+    u--, v--;
+    dist1[u][u] = 0;
+    dist1[v][v] = 0;
+    dist1[u][v] = min(dist1[u][v], l);
+    dist1[v][u] = min(dist1[v][u], l);
+
+    dist2[u][u] = 0;
+    dist2[v][v] = 0;
+    dist2[u][v] = min(dist2[u][v], l);
+    dist2[v][u] = min(dist2[v][u], l);
+    g[u].emplace_back(v, l);
+    g[v].emplace_back(u, l);
+    es.emplace_back(u, v);
+  }
+
+  floyd_warshall();
+  for (auto [u, v] : es) {
+    int ans = oo;
+    if (dist2[u][v] < dist1[u][v])
+      ans = dist2[u][v];
+    else
+      ans = dijkstra(u, v);
+
+    cout << (ans == oo ? -1 : ans) << endl;
   }
 }
 int32_t main(void) {
-  // fastio;
+  fastio;
   int t;
   t = 1;
   // cin >> t;
-  while (t--) run();
+  while (t--) solve();
 }
+
+// AC, floyd warshall, dijkstra
