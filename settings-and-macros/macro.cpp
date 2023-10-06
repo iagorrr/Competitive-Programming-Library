7d0a3be060b4340f5ffb12dc8ffeab15ea79b157
iagorrr04 <iagorrr04@gmail.com>
2023-10-06 20:52:19 -0300

    :technologist: improves macro structure

--- settings-and-macros/macro.cpp
+++ settings-and-macros/macro.cpp
@@ -1,11 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#endif
 #define endl '\n'
 #define fastio                      \
   ios_base::sync_with_stdio(false); \
   cin.tie(0);                       \
   cout.tie(0);
-#define len(__x) (int) __x.size()
+#define len(__x) (int)__x.size()
 using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
@@ -22,16 +25,25 @@ using vc = vector<char>;
 #define mp(___a, ___b) make_pair(___a, ___b)
 #define eb(___x) emplace_back(___x)
 
-vector<string> dir({"LU", "U", "RU", "R", "RD", "D", "LD", "L"});
-int dx[] =           {-1,  -1,   -1,   0,    1,   1,    1,   0};
-int dy[] =           {-1,   0,    1,   1,    1,   0,   -1,  -1};
+// vector<string> dir({"LU", "U", "RU", "R", "RD", "D", "LD", "L"});
+// int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0};
+// int dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+vector<string> dir({"U", "R", "D", "L"});
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
 const ll oo = 1e18;
 
 auto solve() {}
 int32_t main(void) {
+#ifndef LOCAL
   fastio;
+#endif
+
   int t;
   t = 1;
   // cin >> t;
-  while (t--) solve();
+  for (int i = 1; i <= t; i++) {
+    solve();
+  }
 }
