98dff0719e13ebb1f5430ceb63b0d7acda1e649d
iagorrr04 <iagorrr04@gmail.com>
2023-10-10 09:24:32 -0300

    :technologist: improves minimum cut implementation and description

--- algorithms/graphs/minimum-cut-(unweighted).cpp
+++ algorithms/graphs/minimum-cut-(unweighted).cpp
@@ -1,77 +1,92 @@
-const ll oo = 1e17;
+struct Mincut {
+  int n;
+  vi2d g;
+  vii edges;
+  vll2d capacity;
+  vi ps, vis;
 
-ll bfs(int s, int t, vi2d &g, vll2d &capacity, vi &parent) {
-  fill(all(parent), -1);
-  parent[s] = -2;
-  queue<pair<ll, int>> q;
-  q.push({oo, s});
+  Mincut(int _n) : n(_n), g(n), capacity(n, vll(n)), ps(n), vis(n) {}
 
-  while (!q.empty()) {
-    auto [flow, cur] = q.front();
-    q.pop();
+  void add(int u, int v, ll c = 1, bool directed = false, bool set = false) {
+    edges.emplace_back(u, v);
+    g[u].emplace_back(v);
 
-    for (auto next : g[cur]) {
-      if (parent[next] == -1 and capacity[cur][next]) {
-        parent[next] = cur;
-        ll new_flow = min(flow, capacity[cur][next]);
-        if (next == t) return new_flow;
-        q.push({new_flow, next});
+    if (not set)
+      capacity[u][v] += c;
+    else
+      capacity[u][v] = c;
+
+    if (not directed) {
+      g[v].emplace_back(u);
+
+      if (not set)
+        capacity[v][u] += c;
+      else
+        capacity[v][u] = c;
+    }
+  }
+
+  ll bfs(int s, int t) {
+    fill(all(ps), -1);
+    ps[s] = -2;
+    queue<pair<ll, int>> q;
+    q.push({oo, s});
+
+    while (!q.empty()) {
+      auto [flow, cur] = q.front();
+      q.pop();
+
+      for (auto next : g[cur]) {
+        if (ps[next] == -1 and capacity[cur][next]) {
+          ps[next] = cur;
+          ll new_flow = min(flow, capacity[cur][next]);
+          if (next == t) return new_flow;
+          q.push({new_flow, next});
+        }
+      }
+    }
+
+    return 0ll;
+  }
+
+  ll maxflow(int s, int t) {
+    ll flow = 0;
+    ll new_flow;
+
+    while ((new_flow = bfs(s, t))) {
+      flow += new_flow;
+      int cur = t;
+      while (cur != s) {
+        int prev = ps[cur];
+        capacity[prev][cur] -= new_flow;
+        capacity[cur][prev] += new_flow;
+        cur = prev;
+      }
+    }
+
+    return flow;
+  }
+  void dfs(int u) {
+    vis[u] = true;
+
+    for (auto v : g[u]) {
+      if (capacity[u][v] > 0 and not vis[v]) {
+        dfs(v);
       }
     }
   }
 
-  return 0ll;
-}
+  vii mincut(int s, int t) {
+    maxflow(s, t);
+    fill(all(vis), 0);
+    dfs(s);
 
-ll maxflow(int s, int t, int n, vi2d &g, vll2d &capacity) {
-  ll flow = 0;
-  vi parent(n);
-  ll new_flow;
-
-  while ((new_flow = bfs(s, t, g, capacity, parent))) {
-    flow += new_flow;
-    int cur = t;
-    while (cur != s) {
-      int prev = parent[cur];
-      capacity[prev][cur] -= new_flow;
-      capacity[cur][prev] += new_flow;
-      cur = prev;
+    vii removed;
+    for (auto &[u, v] : edges) {
+      if ((vis[u] and not vis[v]) or (vis[v] and not vis[u]))
+        removed.emplace_back(u, v);
     }
+
+    return removed;
   }
-
-  return flow;
-}
-void dfs(int u, vi2d &g, vll2d &capacity, vc &visited) {
-  visited[u] = true;
-
-  for (auto v : g[u]) {
-    if (capacity[u][v] > 0 and not visited[v]) {
-      dfs(v, g, capacity, visited);
-    }
-  }
-}
-
-vii mincut(vii &edges, int s, int t, int n, bool directed = false) {
-  vll2d capacity(n, vll(n));
-  vi2d g(n);
-  for (auto &[u, v] : edges) {
-    g[u].eb(v);
-    capacity[u][v] += 1;
-    if (not directed) {
-      g[v].eb(u);
-      capacity[v][u] += 1;
-    }
-  }
-
-  maxflow(0, n - 1, n, g, capacity);
-  vc vis(n);
-  dfs(0, g, capacity, vis);
-
-  vii removed;
-  for (auto &[u, v] : edges) {
-    if ((vis[u] and not vis[v]) or (vis[v] and not vis[u]))
-      removed.emplace_back(u, v);
-  }
-
-  return removed;
-}
+};
