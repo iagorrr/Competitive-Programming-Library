9fe3a63cbd7ce40093034d2ad9f0b588c8e81f36
iagorrr04 <iagorrr04@gmail.com>
2023-10-12 15:37:11 -0300

    :technologist: improves small to large implementation

--- algorithms/graphs/small_to_large.cpp
+++ algorithms/graphs/small_to_large.cpp
@@ -1,18 +1,20 @@
 struct SmallToLarge {
-  vector<vector<int>> tree, vis_childs;
-  vector<int> sizes, values, ans;
+  int n;
+  vi2d tree, vis_childs;
+  vi sizes, values, ans;
   set<int> cnt;
 
-  SmallToLarge(vector<vector<int>> &&g, vector<int> &&v)
-    : tree(g), vis_childs(g.size()), sizes(g.size()), values(v), ans(g.size()) {
-    update_sizes(0);
+  SmallToLarge(vi2d &g, vi &v)
+    : tree(g), vis_childs(len(g)), sizes(len(g)), values(v), ans(len(g)) {
+    get_size(0);
+    dfs(0);
   }
 
   inline void add_value(int u) { cnt.insert(values[u]); }
 
   inline void remove_value(int u) { cnt.erase(values[u]); }
 
-  inline void update_ans(int u) { ans[u] = (int)cnt.size(); }
+  inline void update_ans(int u) { ans[u] = len(cnt); }
 
   void dfs(int u, int p = -1, bool keep = true) {
     int mx = -1;
@@ -50,13 +52,12 @@ struct SmallToLarge {
     }
   }
 
-  void update_sizes(int u, int p = -1) {
+  void get_size(int u, int p = -1) {
     sizes[u] = 1;
-    for (auto x : tree[u]) {
+    for (auto x : tree[u])
       if (x != p) {
-        update_sizes(x, u);
+        get_size(x, u);
         sizes[u] += sizes[x];
       }
-    }
   }
 };
