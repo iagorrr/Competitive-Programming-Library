7694181dd72bd325cfcc9a86b78bae0cb7226d38
iagorrr04 <iagorrr04@gmail.com>
2023-10-16 21:15:57 -0300

    :fire: removes duplicated binary lifting

--- algorithms/graphs/binary-lifting.cpp
+++
@@ -1,8 +0,0 @@
-const int maxlog = 20;
-int far[maxlog + 1][n + 1];
-int n;
-for (int h = 1; h <= maxlog; h++) {
-  for (int i = 1; i <= n; i++) {
-    far[h][i] = far[h - 1][far[h - 1][i]];
-  }
-}
