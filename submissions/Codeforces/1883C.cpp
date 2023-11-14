a9d3d05eb8b3df1537fa6b0f530883e9c1a48d26
iagorrr04 <iagorrr04@gmail.com>
2023-11-14 12:50:02 -0300

    :white_check_mark: math, number theory

---
+++ submissions/Codeforces/1883C.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...) 42
+#endif
+#define endl '\n'
+#define fastio                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define len(__x) (int)__x.size()
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+
+// vector<string> dir({"LU", "U", "RU", "R", "RD", "D",
+// "LD", "L"}); int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0}; int
+// dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+vector<string> dir({"U", "R", "D", "L"});
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const ll oo = 1e18;
+int T(1);
+
+auto solve() {
+  int n, k;
+  cin >> n >> k;
+
+  int ans = k;
+
+  int cntodd, cnteven;
+  cntodd = cnteven = 0;
+  for (int i = 0; i < n; i++) {
+    int ai;
+    cin >> ai;
+    cntodd += ai & 1, cnteven += !(ai & 1);
+
+    if (ai % k == 0) ans = 0;
+    ans = min(ans, k - (ai % k));
+  }
+
+  if (k == 4) {
+    ans = min(ans, 2 - min(2, cnteven));
+  }
+  cout << ans << endl;
+}
+
+int32_t main(void) {
+#ifndef LOCAL
+  fastio;
+#endif
+
+  cin >> T;
+
+  for (int i = 1; i <= T; i++) {
+    solve();
+  }
+}
+
+// AC,math, number theory
