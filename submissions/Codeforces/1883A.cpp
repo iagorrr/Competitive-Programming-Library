43e2cf20b93f83746fb0b3da11f6478768c705ce
iagorrr04 <iagorrr04@gmail.com>
2023-11-14 12:48:44 -0300

    :white_check_mark: math

---
+++ submissions/Codeforces/1883A.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...) 42
+#endif
+#define endl '\n'
+#define fastio                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define len(__x) (int)__x.size()
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+
+// vector<string> dir({"LU", "U", "RU", "R", "RD", "D",
+// "LD", "L"}); int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0}; int
+// dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+vector<string> dir({"U", "R", "D", "L"});
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const ll oo = 1e18;
+int T(1);
+
+auto solve() {
+  string s;
+  cin >> s;
+
+  map<char, int> pos;
+  for (int i = 1; i <= 9; i++) {
+    pos['0' + i] = i;
+  }
+  pos['0'] = 10;
+
+  char cur = '1';
+  int ans = 0;
+  for (auto c : s) {
+    ans += abs(pos[c] - pos[cur]) + 1;
+    cur = c;
+  }
+
+  cout << ans << endl;
+}
+
+int32_t main(void) {
+#ifndef LOCAL
+  fastio;
+#endif
+
+  cin >> T;
+
+  for (int i = 1; i <= T; i++) {
+    solve();
+  }
+}
+
+// AC, math
