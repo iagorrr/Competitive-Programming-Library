fb0f6e378d4de3a3e083feef504b6802e28b2397
iagorrr04 <iagorrr04@gmail.com>
2023-11-28 11:54:51 -0300

    :technologist: improves prim implementation

--- algorithms/graphs/prim.cpp
+++ algorithms/graphs/prim.cpp
@@ -1,18 +1,20 @@
-const int maxn(1'00'000);
-vector<pair<ll, int>> g[maxn];
-int n;
-
-ll prim(int s = 0) {
+const int MAXN(1'00'000);
+int N;
+vector<pair<ll, int>> G[MAXN];
+ll prim(vi s = vi(1, 0)) {
   priority_queue<pair<ll, int>, vector<pair<ll, int>>,
                  greater<pair<ll, int>>>
     pq;
-  vector<char> ingraph(maxn);
-  ingraph[s] = true;
-  int ingraphcnt = 1;
-  for (auto &[w, v] : g[s]) pq.emplace(w, v);
+  vector<char> ingraph(MAXN);
+  int ingraphcnt(0);
+  for (auto si : s) {
+    ingraphcnt++;
+    ingraph[si] = true;
+    for (auto &[w, v] : G[si]) pq.emplace(w, v);
+  }
 
   ll mstcost = 0;
-  while (ingraphcnt < n) {
+  while (ingraphcnt < N) {
     ll w;
     int v;
 
@@ -23,10 +25,10 @@ ll prim(int s = 0) {
 
     mstcost += w, ingraph[v] = true, ingraphcnt++;
 
-    for (auto &[w2, v2] : g[v]) {
+    for (auto &[w2, v2] : G[v]) {
       pq.emplace(w2, v2);
     }
   }
 
-  return ingraphcnt == n ? mstcost : oo;
+  return ingraphcnt == N ? mstcost : oo;
 }
