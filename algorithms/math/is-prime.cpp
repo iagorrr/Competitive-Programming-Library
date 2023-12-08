46c17421cb344abdbf7e4c0ba00408fb86d0c956
iagorrr04 <iagorrr04@gmail.com>
2023-12-07 22:21:35 -0300

    :fire: removes old implementation for is prime

--- algorithms/math/is-prime.cpp
+++
@@ -1,8 +0,0 @@
-bool isprime(ll n) {
-  if (n < 2) return false;
-  if (n == 2) return true;
-  if (n % 2 == 0) return false;
-  for (ll i = 3; i * i < n; i += 2)
-    if (n % i == 0) return false;
-  return true;
-}
