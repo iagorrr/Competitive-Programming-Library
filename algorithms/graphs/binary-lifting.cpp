7c76a1cdf59877e87952b30e299244e349e24f52
iagorrr04 <iagorrr04@gmail.com>
2023-12-11 14:29:36 -0300

    :technologist: improves binary jumping implementation and description

---
+++ algorithms/graphs/binary-lifting.cpp
@@ -0,0 +1,17 @@
+const int MAXN(2e5), MAXLOG2(30);
+int bl[MAXN][MAXLOG2 + 1];
+int N;
+
+int jump(int u, ll k) {
+  for (int i = 0; i <= MAXLOG2; i++) {
+    if (k & (1ll << i)) u = bl[u][i];
+  }
+  return u;
+}
+void build() {
+  for (int i = 1; i <= MAXLOG2; i++) {
+    for (int j = 0; j < N; j++) {
+      bl[j][i] = bl[bl[j][i - 1]][i - 1];
+    }
+  }
+}
