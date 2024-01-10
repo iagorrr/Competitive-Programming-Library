0c81128d4bebe0483e0ec44bea7ab08292e3a0cd
iagorrr04 <iagorrr04@gmail.com>
2024-01-09 21:36:03 -0300

    :white_check_mark: greedy, math

---
+++ submissions/Codeforces/1898B.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...) 42
+#endif
+#define endl '\n'
+#define fastio                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define len(__x) (int)__x.size()
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+int lg2(ll x) {
+  return __builtin_clzll(1) - __builtin_clzll(x);
+}
+
+// vector<string> dir({"LU", "U", "RU", "R", "RD", "D",
+// "LD", "L"}); int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0}; int
+// dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+vector<string> dir({"U", "R", "D", "L"});
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const ll oo = 1e18;
+int T(1);
+const int MAXN(1'000'000);
+
+auto run() {
+  int n;
+  cin >> n;
+  vll xs(n);
+  for (auto &xi : xs) {
+    cin >> xi;
+  }
+
+  ll ans = 0;
+  for (int i = n - 1; i > 0; i--) {
+    ll cur = xs[i];
+    ll prev = xs[i - 1];
+    if (cur >= prev) continue;
+    ll k = (prev + cur - 1) / cur;
+    ans += k-1; 
+    xs[i - 1] = prev/k;
+  }
+
+  cout << ans << '\n';
+}
+
+int32_t main(void) {
+#ifndef LOCAL
+  fastio;
+#endif
+
+  cin >> T;
+
+  for (int i = 1; i <= T; i++) {
+    run();
+  }
+}
+
+/*
+ * AC
+ * greedy
+ * math
+ * https://codeforces.com/problemset/problem/1898/B
+ * */
