809e2774594dedbd51c67c066dccad5ab6250d0d
iagorrr04 <iagorrr04@gmail.com>
2024-01-14 11:38:50 -0300

    :white_check_mark: strings, combinatorics

---
+++ submissions/atCoder/abc336_c.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...) 42
+#endif
+#define endl '\n'
+#define fastio                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define len(__x) (int)__x.size()
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+int lg2(ll x) {
+  return __builtin_clzll(1) - __builtin_clzll(x);
+}
+
+// vector<string> dir({"LU", "U", "RU", "R", "RD", "D",
+// "LD", "L"}); int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0}; int
+// dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+vector<string> dir({"U", "R", "D", "L"});
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const ll oo = 1e18;
+int T(1);
+const int MAXN(1'000'000);
+
+auto run() {
+  ll n;
+  cin >> n;
+
+  if (n == 1) {
+    cout << '0' << '\n';
+    return;
+  }
+
+  string dgts("02468");
+  string ans;
+  ll p5 = 1;
+  ll N = 25;
+  for (int i = 0; i < N; i++) {
+    p5 *= 5;
+  }
+
+  ll k = 0;
+  p5 /= 5;
+  for (int i = 0; i < N; i++) {
+    char c = '#';
+    for (int j = 1; j < len(dgts); j++) {
+      if (k + p5 >= n) break;
+      c = dgts[j];
+      k += p5;
+    }
+    if (c != '#')
+      ans.push_back(c);
+    else if (!ans.empty())
+      ans.push_back('0');
+    p5 /= 5;
+  }
+
+  cout << ans << '\n';
+}
+
+int32_t main(void) {
+#ifndef LOCAL
+  fastio;
+#endif
+
+  // cin >> T;
+
+  for (int i = 1; i <= T; i++) {
+    run();
+  }
+}
