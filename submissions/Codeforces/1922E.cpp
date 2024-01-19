5269081aa9e83ab290f896c7af945a237c223296
iagorrr04 <iagorrr04@gmail.com>
2024-01-19 12:57:27 -0300

    :white_check_mark: math, constructive, bitmasks

---
+++ submissions/Codeforces/1922E.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...) 42
+#endif
+#define endl '\n'
+#define fastio                      \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define len(__x) (int)__x.size()
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define pb(___x) push_back(___x)
+#define mp(___a, ___b) make_pair(___a, ___b)
+#define eb(___x) emplace_back(___x)
+int lg2(ll x) {
+  return __builtin_clzll(1) - __builtin_clzll(x);
+}
+
+// vector<string> dir({"LU", "U", "RU", "R", "RD", "D",
+// "LD", "L"}); int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0}; int
+// dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+vector<string> dir({"U", "R", "D", "L"});
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const ll oo = 1e18;
+int T(1);
+const int MAXN(1'000'000);
+
+auto run() {
+  ll x;
+  cin >> x;
+
+  vll bits;
+  for (ll i = 0; (1ll << i) <= x; i++) {
+    if (x & (1ll << i)) bits.emplace_back(i);
+  }
+
+  vll tempans;
+  for (int i = 1; i <= bits.back(); i++) {
+    tempans.emplace_back(i);
+  }
+  bits.pop_back();
+
+  int atRight = len(tempans);
+  ll down = -1;
+  vll ans;
+  for (int i = 0; i < len(tempans); i++) {
+    if (not bits.empty() and atRight == bits.back()) {
+      ans.emplace_back(down--);
+      bits.pop_back();
+    }
+    ans.emplace_back(tempans[i]);
+    atRight--;
+  }
+
+  if (not bits.empty()) {
+    ans.emplace_back(down--);
+  }
+
+  cout << len(ans) << '\n';
+  for (int i = 0; i < len(ans); i++) {
+    cout << ans[i] << " \n"[i + 1 == len(ans)];
+  }
+}
+
+int32_t main(void) {
+#ifndef LOCAL
+  fastio;
+#endif
+
+  cin >> T;
+
+  for (int i = 1; i <= T; i++) {
+    run();
+  }
+}
+
+/*
+ * bitmasks
+ * constructive
+ * math
+ * combinatorics
+ * https://codeforces.com/contest/1922/problem/E
+ * */
