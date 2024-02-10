6b269ece48fc0931361ac9a441ce0bd12200c703
iagorrr04 <iagorrr04@gmail.com>
2024-02-10 20:06:56 -0300

    :technologist: improves algorithm to find diophantine equation solutions

--- algorithms/math/find-solution-diophantine-equation.cpp
+++ algorithms/math/find-solution-diophantine-equation.cpp
@@ -1,25 +1,49 @@
-ll gcd(ll a, ll b, ll &x, ll &y) {
-  if (b == 0) {
-    x = 1;
-    y = 0;
-    return a;
-  }
-  ll x1, y1;
-  ll d = gcd(b, a % b, x1, y1);
-  x = y1;
-  y = x1 - y1 * (a / b);
-  return d;
+template <typename T>
+tuple<T, T, T> ext_gcd(T a, T b) {
+  if (b == 0) return {a, 1, 0};
+
+  auto [d, x1, y1] = ext_gcd(b, a % b);
+
+  return {d, y1, x1 - y1 * (a / b)};
 }
 
-bool find_any_solution(ll a, ll b, ll c, ll &x0, ll &y0) {
-  auto g = gcd(a < 0 ? -a : a, b < 0 ? -b : b, x0, y0);
-  if (c % g) {
-    return false;
-  }
+template <typename T>
+tuple<bool, T, T> find_any_solution(T a, T b, T c) {
+  assert(a != 0 or b != 0);
+#warning Be careful with overflow, use __int128 if needed !
 
-  x0 *= c / g;
-  y0 *= c / g;
+  auto [d, x0, y0] =
+    ext_gcd(a < 0 ? -a : a, b < 0 ? -b : b);
+
+  if (c % d) return {false, 0, 0};
+
+  x0 *= c / d;
+  y0 *= c / d;
   if (a < 0) x0 = -x0;
   if (b < 0) y0 = -y0;
-  return true;
+
+  return {true, x0, y0};
+}
+
+// optional if you want to use __int128
+void print(__int128 x) {
+  if (x < 0) {
+    cout << '-';
+    x = -x;
+  }
+  if (x > 9) print(x / 10);
+  cout << (char)((x % 10) + '0');
+}
+
+__int128 read() {
+  string s;
+  cin >> s;
+  __int128 x = 0;
+  for (auto c : s) {
+    if (c != '-') x += c - '0';
+    x *= 10;
+  }
+  x /= 10;
+  if (s[0] == '-') x = -x;
+  return x;
 }
