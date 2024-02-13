28acb158aaca4afdbde543d2aaeda3fa5f9a63df
iagorrr04 <iagorrr04@gmail.com>
2024-02-13 15:38:56 -0300

    :white_check_mark: ad-hoc

---
+++ submissions/Codeforces/1931C.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define endl '\n'
+#define fastio                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);
+// #define int long long
+#define len(__x) (int)__x.size()
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define rep(i, begin, end)                              \
+  for (__typeof(begin) i = (begin) - ((begin) > (end)); \
+       i != (end) - ((begin) > (end));                  \
+       i += 1 - 2 * ((begin) > (end)))
+
+int lg2(ll x) {
+  return __builtin_clzll(1) - __builtin_clzll(x);
+}
+
+// vector<string> dir({"LU", "U", "RU", "R", "RD", "D",
+// "LD", "L"}); int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0}; int
+// dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+vector<string> dir({"U", "R", "D", "L"});
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const ll oo = 1e18;
+int T(1);
+const int MAXN(1'000'000);
+
+auto run() {
+  int n;
+  cin >> n;
+
+  vll xs(n);
+  for (auto &xi : xs) cin >> xi;
+
+  ll pref = 0;
+  while (pref < n and xs[0] == xs[pref]) pref++;
+  pref--;
+
+  int suff = n - 1;
+  while (suff >= 0 and xs.back() == xs[suff]) suff--;
+  suff++;
+
+  dbg(pref, suff);
+
+  int ans = 0;
+  if (xs.back() == xs.front()) {
+    if (pref == n - 1)
+      ans = 0;
+    else {
+      int i = pref + 1;
+      int j = suff - 1;
+      ans = j - i + 1;
+    }
+  }
+  else {
+    ans = min(n - (pref + 1), (ll)suff);
+  }
+
+  cout << ans << '\n';
+}
+
+int32_t main(void) {
+#ifndef LOCAL
+  fastio;
+#endif
+
+  cin >> T;
+
+  for (int i = 1; i <= T; i++) {
+    run();
+  }
+}
+
+// AC, ad-hoc
