77679805e0ec0ce723ba763267645ee9f1b93229
iagorrr04 <iagorrr04@gmail.com>
2024-02-13 15:37:47 -0300

    :white_check_mark: brute force

---
+++ submissions/Codeforces/1931A.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define endl '\n'
+#define fastio                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);
+// #define int long long
+#define len(__x) (int)__x.size()
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define rep(i, begin, end)                              \
+  for (__typeof(begin) i = (begin) - ((begin) > (end)); \
+       i != (end) - ((begin) > (end));                  \
+       i += 1 - 2 * ((begin) > (end)))
+
+int lg2(ll x) {
+  return __builtin_clzll(1) - __builtin_clzll(x);
+}
+
+// vector<string> dir({"LU", "U", "RU", "R", "RD", "D",
+// "LD", "L"}); int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0}; int
+// dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+vector<string> dir({"U", "R", "D", "L"});
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const ll oo = 1e18;
+int T(1);
+const int MAXN(1'000'000);
+
+auto run() {
+  int K;
+  cin >> K;
+
+  string ans = "zzzzzzz";
+  for (char i = 'a'; i <= 'z'; i++) {
+    for (char j = 'a'; j <= 'z'; j++) {
+      for (char k = 'a'; k <= 'z'; k++) {
+        string s;
+        s.pb(i);
+        s.pb(j);
+        s.pb(k);
+        // cout << ((int)i + (int)j + (int)k) << '\n';
+        if (((int)i + (int)j + (int)k) - 3 * 'a' + 3 == K) {
+          // dbg(i, j, k);
+          ans = min(ans, s);
+        }
+      }
+    }
+  }
+
+  cout << ans << '\n';
+}
+
+int32_t main(void) {
+#ifndef LOCAL
+  fastio;
+#endif
+
+  cin >> T;
+
+  for (int i = 1; i <= T; i++) {
+    run();
+  }
+}
+
+// AC, brute force
