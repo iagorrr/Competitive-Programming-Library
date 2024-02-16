cd7a585f48d54ff8ecbfd418333078dbc3ae12af
iagorrr04 <iagorrr04@gmail.com>
2024-02-15 23:36:43 -0300

    :sparkles: adds derangement code

---
+++ algorithms/math/derangement.cpp
@@ -0,0 +1,37 @@
+#warning Remember to call precompute !
+const ll MOD = 1e9 + 7;
+const int MAXN(1'000'000);
+ll fats[MAXN + 1];
+void precompute() {
+  fats[0] = 1;
+  for (ll i = 1; i <= MAXN; i++) {
+    fats[i] = (fats[i - 1] * i) % MOD;
+  }
+}
+
+ll fastpow(ll a, ll p, ll m) {
+  ll ret = 1;
+  while (p) {
+    if (p & 1) ret = (ret * a) % MOD;
+    p >>= 1;
+    a = (a * a) % MOD;
+  }
+  return ret;
+}
+
+ll divmod(ll a, ll b) {
+  return (a * fastpow(b, MOD - 2, MOD)) % MOD;
+}
+
+ll derangement(const ll n) {
+  ll ans = fats[n];
+  for (ll i = 1; i <= n; i++) {
+    ll k = divmod(fats[n], fats[i]);
+    if (i & 1) {
+      ans = (ans - k + MOD) % MOD;
+    } else {
+      ans = (ans + k) % MOD;
+    }
+  }
+  return ans;
+}
