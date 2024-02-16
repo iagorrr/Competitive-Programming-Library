f8b8cb303b986fe385883247fbf6f83bd811ce95
iagorrr04 <iagorrr04@gmail.com>
2024-02-15 23:32:42 -0300

    :white_check_mark: derangement

---
+++ submissions/CSES/1717 - Christmas Party.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define endl '\n'
+#define fastio                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);
+// #define int long long
+#define len(__x) (int)__x.size()
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define rep(i, begin, end)                              \
+  for (__typeof(begin) i = (begin) - ((begin) > (end)); \
+       i != (end) - ((begin) > (end));                  \
+       i += 1 - 2 * ((begin) > (end)))
+
+int lg2(ll x) {
+  return __builtin_clzll(1) - __builtin_clzll(x);
+}
+
+// vector<string> dir({"LU", "U", "RU", "R", "RD", "D",
+// "LD", "L"}); int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0}; int
+// dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+vector<string> dir({"U", "R", "D", "L"});
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const ll oo = 1e18;
+int T(1);
+
+const ll MOD = 1e9 + 7;
+const int MAXN(1'000'000);
+ll fats[MAXN + 1];
+void precompute() {
+  fats[0] = 1;
+  for (ll i = 1; i <= MAXN; i++) {
+    fats[i] = (fats[i - 1] * i) % MOD;
+  }
+}
+
+ll fastpow(ll a, ll p, ll m) {
+  ll ret = 1;
+  while (p) {
+    if (p & 1) ret = (ret * a) % MOD;
+    p >>= 1;
+    a = (a * a) % MOD;
+  }
+  return ret;
+}
+
+ll divmod(ll a, ll b) {
+  return (a * fastpow(b, MOD - 2, MOD)) % MOD;
+}
+
+ll derangement(const ll n) {
+  ll ans = fats[n];
+  for (ll i = 1; i <= n; i++) {
+    ll k = divmod(fats[n], fats[i]);
+    if (i & 1) {
+      ans = (ans - k + MOD) % MOD;
+    } else {
+      ans = (ans + k) % MOD;
+    }
+  }
+  return ans;
+}
+
+auto run() {
+  ll n;
+  cin >> n;
+  cout << derangement(n) << '\n';
+}
+
+int32_t main(void) {
+#ifndef LOCAL
+  fastio;
+#endif
+
+  precompute();
+  // cin >> T;
+
+  for (int t = 1; t <= T; t++) {
+    run();
+  }
+}
+
+// AC, combinatorics, math, derangement
