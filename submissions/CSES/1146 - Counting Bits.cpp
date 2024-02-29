9d61f034aebedc471d49f28f2388d082c61afc1e
iagorrr04 <iagorrr04@gmail.com>
2024-02-28 22:25:34 -0300

    :white_check_mark: bit manipulation

---
+++ submissions/CSES/1146 - Counting Bits.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#include "debug.cpp"
+#else
+#define dbg(...)
+#endif
+#define endl '\n'
+#define fastio                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);
+// #define int long long
+#define len(__x) (int)__x.size()
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vll2d = vector<vll>;
+using vi = vector<int>;
+using vi2d = vector<vi>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using vc = vector<char>;
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define rep(i, begin, end)                              \
+  for (__typeof(begin) i = (begin) - ((begin) > (end)); \
+       i != (end) - ((begin) > (end));                  \
+       i += 1 - 2 * ((begin) > (end)))
+
+int lg2(ll x) {
+  return __builtin_clzll(1) - __builtin_clzll(x);
+}
+
+// vector<string> dir({"LU", "U", "RU", "R", "RD", "D",
+// "LD", "L"}); int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0}; int
+// dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+vector<string> dir({"U", "R", "D", "L"});
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const ll OO = 1e18;
+const int oo = 1e9;
+
+int T(1);
+
+auto run() {
+  ll n;
+  cin >> n;
+
+  const int MAXBIT = 60;
+
+  ll ans = 0;
+  n++;
+  for (ll p2 = 2; (p2 >> 1ll) <= n; p2 <<= 1ll) {
+    ans +=
+      n / p2 * (p2 >> 1ll) + max(0ll, n % p2 - (p2 >> 1ll));
+  }
+
+  cout << ans << '\n';
+}
+
+int32_t main(void) {
+#ifndef LOCAL
+  fastio;
+#endif
+
+  // cin >> T;
+
+  for (int t = 1; t <= T; t++) {
+    run();
+  }
+}
+
+// AC, bit manipulation
