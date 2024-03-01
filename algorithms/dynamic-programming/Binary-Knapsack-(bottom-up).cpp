470470ea3e08a8411d496efee5cf9785a8e0e89c
iagorrr04 <iagorrr04@gmail.com>
2024-03-01 16:09:57 -0300

    :technologist: improves knapsack implementation to allow elements with cost 0

--- algorithms/dynamic-programming/Binary-Knapsack-(bottom-up).cpp
+++ algorithms/dynamic-programming/Binary-Knapsack-(bottom-up).cpp
@@ -1,37 +1,38 @@
-const int MAXN(2010), MAXM(2010);
-ll st[MAXN + 1][MAXM + 1];
-char ps[MAXN + 1][MAXM + 1];
-pair<ll, vi> knapsack(int M, const vll &VS, const vi &WS) {
-  memset(st, 0, sizeof(st));
-  memset(ps, 0, sizeof(ps));
-  int N = len(VS) - 1;  // ELEMENTS START AT INDEX 1 !
+const int MAXN(1'000), MAXCOST(1'000 * 20);
+ll dp[MAXN + 1][MAXCOST + 1];
+bool ps[MAXN + 1][MAXCOST + 1];
+pair<ll, vi> knapsack(const vll &points, const vi &costs,
+                      int maxCost) {
+  int n = len(points) - 1;  // ELEMENTS START AT INDEX 1 !
 
-  for (int i = 0; i <= N; ++i) st[i][0] = 0;
+  for (int m = 0; m <= maxCost; m++) {
+    dp[0][m] = 0;
+  }
 
-  for (int m = 0; m <= M; ++m) st[0][m] = 0;
+  for (int i = 1; i <= n; i++) {
+    dp[i][0] = dp[i - 1][0] + (costs[i] == 0) * points[i];
+    ps[i][0] = costs[i] == 0;
+  }
 
-  for (int i = 1; i <= N; ++i) {
-    for (int m = 1; m <= M; ++m) {
-      st[i][m] = st[i - 1][m];
-      ps[i][m] = 0;
-      int w = WS[i];
-      ll v = VS[i];
+  for (int i = 1; i <= n; i++) {
+    for (int m = 1; m <= maxCost; m++) {
+      dp[i][m] = dp[i - 1][m], ps[i][m] = 0;
+      int w = costs[i];
+      ll v = points[i];
 
-      if (w <= m and st[i - 1][m - w] + v > st[i][m]) {
-        st[i][m] = st[i - 1][m - w] + v;
-        ps[i][m] = 1;
+      if (w <= m and dp[i - 1][m - w] + v > dp[i][m]) {
+        dp[i][m] = dp[i - 1][m - w] + v, ps[i][m] = 1;
       }
     }
   }
 
-  int m = M;
   vi is;
-  for (int i = N; i >= 1; --i) {
+  for (int i = n, m = maxCost; i >= 1; --i) {
     if (ps[i][m]) {
-      is.emplace_back(i - 1);
-      m -= WS[i];
+      is.emplace_back(i);
+      m -= costs[i];
     }
   }
 
-  return {st[N][M], is};
+  return {dp[n][maxCost], is};
 }
