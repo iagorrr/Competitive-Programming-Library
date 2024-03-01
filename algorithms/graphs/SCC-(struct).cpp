669faf0a06e9d6f2a1688827f6312ab9ddc2889f
iagorrr04 <iagorrr04@gmail.com>
2024-03-01 16:28:08 -0300

    :technologist: changes scc struct to use low link instead kosajaru

--- algorithms/graphs/SCC-(struct).cpp
+++ algorithms/graphs/SCC-(struct).cpp
@@ -1,47 +1,33 @@
 struct SCC {
-  int N, totscc;
-  vi2d g, tg;
-  vi todo, comp;
+  int num_sccs = 0;
+  vi scc_id;
   vector<set<ll>> gscc;
-  vc vis;
-  SCC(int _N)
-    : N(_N),
-      totscc(0),
-      g(_N),
-      tg(_N),
-      comp(_N, -1),
-      gscc(_N),
-      vis(_N) {}
 
-  void add_edge(int x, int y) { g[x].eb(y), tg[y].eb(x); }
-
-  void dfs(int x) {
-    vis[x] = 1;
-    for (auto y : g[x])
-      if (!vis[y]) dfs(y);
-    todo.pb(x);
-  }
-
-  void dfs2(ll x) {
-    comp[x] = totscc;
-    for (auto y : tg[x])
-      if (comp[y] == -1) dfs2(y);
-  }
-
-  void build() {
-    for (int i = 0; i < N; ++i)
-      if (!vis[i]) dfs(i);
-
-    reverse(all(todo));
-    for (auto &x : todo)
-      if (comp[x] == -1) {
-        dfs2(x);
-        totscc++;
+  SCC(const vi2d& adj)
+    : scc_id(len(adj), -1), gscc(len(adj)) {
+    int n = len(adj), timer = 1;
+    vi tin(n), st;
+    st.reserve(n);
+    auto dfs = [&](auto&& self, int u) -> int {
+      int low = tin[u] = timer++, siz = len(st);
+      st.push_back(u);
+      for (int v : adj[u])
+        if (scc_id[v] < 0)
+          low = min(low, tin[v] ? tin[v] : self(self, v));
+      if (tin[u] == low) {
+        for (int i = siz; i < len(st); i++)
+          scc_id[st[i]] = num_sccs;
+        st.resize(siz);
+        num_sccs++;
       }
+      return low;
+    };
+    for (int i = 0; i < n; i++)
+      if (!tin[i]) dfs(dfs, i);
 
-    for (int i = 0; i < N; ++i)
-      for (auto j : g[i])
-        if (comp[i] != comp[j])
-          gscc[comp[i]].insert(comp[j]);
+    for (int i = 0; i < len(adj); ++i)
+      for (auto j : adj[i])
+        if (scc_id[i] != scc_id[j])
+          gscc[scc_id[i]].emplace(scc_id[j]);
   }
 };
