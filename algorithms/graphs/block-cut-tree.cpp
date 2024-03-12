2b1a4fa77b03bc040f0e5286e4e6d36c603aff76
iagorrr04 <iagorrr04@gmail.com>
2024-03-12 15:12:10 -0300

    :technologist: improves block cut tree implementation

--- algorithms/graphs/block-cut-tree.cpp
+++ algorithms/graphs/block-cut-tree.cpp
@@ -1,70 +1,57 @@
-// O(n + m)
-struct BlockCutTree {
-  vector<vector<int>> blocks, tree;
-  vector<vector<pair<int, int>>> block_edges;
-  vector<int> articulation, pos;
-
-  BlockCutTree(const vector<vector<int>> &g)
-    : articulation(g.size()), pos(g.size()) {
-    int t = 0;
-    vector<int> id(g.size(), -1);
-    stack<int> s1;
-    stack<pair<int, int>> s2;
-    for (int i = 0; i < (int)g.size(); i++)
-      if (id[i] == -1) dfs(g, i, -1, t, id, s1, s2);
-
-    tree.resize(blocks.size());
-    for (int i = 0; i < (int)g.size(); i++)
-      if (articulation[i])
-        pos[i] = (int)tree.size(), tree.emplace_back();
-
-    for (int i = 0; i < (int)blocks.size(); i++) {
-      for (auto j : blocks[i]) {
-        if (not articulation[j])
-          pos[j] = i;
-        else
-          tree[i].push_back(pos[j]),
-            tree[pos[j]].push_back(i);
-      }
-    }
-  }
-
- private:
-  int dfs(const vector<vector<int>> &g, int i, int p,
-          int &t, vector<int> &id, stack<int> &s1,
-          stack<pair<int, int>> &s2) {
-    int lo = id[i] = t++;
-    s1.push(i);
-
-    if (p != -1) s2.emplace(i, p);
-    for (auto j : g[i])
-      if (j != p and id[j] != -1) s2.emplace(i, j);
-
-    for (auto j : g[i])
-      if (j != p) {
-        if (id[j] == -1) {
-          int val = dfs(g, j, i, t, id, s1, s2);
-          lo = min(lo, val);
-
-          if (val >= id[i]) {
-            articulation[i]++;
-            blocks.emplace_back(1, i);
-            for (; blocks.back().back() != j; s1.pop())
-              blocks.back().push_back(s1.top());
-
-            block_edges.emplace_back(1, s2.top());
-            s2.pop();
-            for (; block_edges.back().back() !=
-                   make_pair(j, i);
-                 s2.pop())
-              block_edges.back().push_back(s2.top());
-          }
-        } else {
-          lo = min(lo, id[j]);
+struct block_cut_tree {
+    int n;
+    vector<int> id, is_cutpoint, tin, low, stk;
+    vector<vector<int>> comps, tree;
+    block_cut_tree(vector<vector<int>> &g) : n(g.size()), id(n), is_cutpoint(n), tin(n), low(n) {
+        // build comps
+        for (int i = 0; i < n; i++) {
+            if (!tin[i]) {
+                int timer = 0;
+                dfs(i, -1, timer, g);
+            }
         }
-      }
 
-    if (p == -1 and articulation[i]) --articulation[i];
-    return lo;
-  }
+        int node_id = 0;
+        for (int u = 0; u < n; u++) {
+            if (is_cutpoint[u]) {
+                id[u] = node_id++;
+                tree.push_back({});
+            }
+        }
+
+        for (auto &comp : comps) {
+            int node = node_id++;
+            tree.push_back({});
+            for (int u : comp) {
+                if (!is_cutpoint[u]) {
+                    id[u] = node;
+                } else {
+                    tree[node].emplace_back(id[u]);
+                    tree[id[u]].emplace_back(node);
+                }
+            }
+        }
+    }
+
+    void dfs(int u, int p, int &timer, vector<vector<int>> &g) {
+        tin[u] = low[u] = ++timer;
+        stk.emplace_back(u);
+
+        for (auto v : g[u]) {
+            if (v == p) continue;
+            if (!tin[v]) {
+                dfs(v, u, timer, g);
+                low[u] = min(low[u], low[v]);
+                if (low[v] >= tin[u]) {
+                    is_cutpoint[u] = (tin[u] > 1 or tin[v] > 2);
+                    comps.push_back({u});
+                    while (comps.back().back() != v) {
+                        comps.back().emplace_back(stk.back());
+                        stk.pop_back();
+                    }
+                }
+            } else
+                low[u] = min(low[u], tin[v]);
+        }
+    }
 };
