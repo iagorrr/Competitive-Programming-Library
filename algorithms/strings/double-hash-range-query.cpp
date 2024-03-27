c1c88420f8417a10c80ddf92fbb6a9b7bf0e290f
iagorrr <iagorrr@gmail.com>
2024-03-27 09:35:26 -0300

    :technologist: imroves double hash range query implementation

--- algorithms/strings/double-hash-range-query.cpp
+++ algorithms/strings/double-hash-range-query.cpp
@@ -1,53 +1,40 @@
+using ll = long long;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+
+const int MAXN(1'000'000);
 const ll MOD = 1000027957;
-const int MOD2 = 1000015187;
+const ll MOD2 = 1000015187;
+const ll P = 31;
+
+ll p[MAXN + 1], p2[MAXN + 1];
+void precompute() {
+	p[0] = p2[0] = 1;
+	for (int i = 1; i <= MAXN; i++) p[i] = (P * p[i - 1]) % MOD, p2[i] = (P * p2[i - 1]) % MOD2;
+}
 
 struct Hash {
-  const ll P = 31;
-  int n;
-  string s;
-  vll h, h2, hi, hi2, p, p2;
-  Hash() {}
-  Hash(string _s)
-    : s(_s),
-      n(len(_s)),
-      h(n),
-      h2(n),
-      hi(n),
-      hi2(n),
-      p(n),
-      p2(n) {
-    for (int i = 0; i < n; i++)
-      p[i] = (i ? P * p[i - 1] : 1) % MOD;
-    for (int i = 0; i < n; i++)
-      p2[i] = (i ? P * p2[i - 1] : 1) % MOD2;
-    for (int i = 0; i < n; i++)
-      h[i] = (s[i] + (i ? h[i - 1] : 0) * P) % MOD;
-    for (int i = 0; i < n; i++)
-      h2[i] = (s[i] + (i ? h2[i - 1] : 0) * P) % MOD2;
-    for (int i = n - 1; i >= 0; i--)
-      hi[i] =
-        (s[i] + (i + 1 < n ? hi[i + 1] : 0) * P) % MOD;
-    for (int i = n - 1; i >= 0; i--)
-      hi2[i] =
-        (s[i] + (i + 1 < n ? hi2[i + 1] : 0) * P) % MOD2;
-  }
-  pii query(int l, int r) {
-    ll hash =
-      (h[r] - (l ? h[l - 1] * p[r - l + 1] % MOD : 0));
-    ll hash2 =
-      (h2[r] - (l ? h2[l - 1] * p2[r - l + 1] % MOD2 : 0));
+	int n;
+	vll h, h2, hi, hi2;
+	Hash() {}
+	Hash(const string& s) : n(s.size()), h(n), h2(n), hi(n), hi2(n) {
+		h[0] = h2[0] = s[0];
+		for (int i = 1; i < n; i++)
+			h[i] = (s[i] + h[i - 1] * P) % MOD, h2[i] = (s[i] + h2[i - 1] * P) % MOD2;
 
-    return {(hash < 0 ? hash + MOD : hash),
-            (hash2 < 0 ? hash2 + MOD2 : hash2)};
-  }
-  pii query_inv(int l, int r) {
-    ll hash =
-      (hi[l] -
-       (r + 1 < n ? hi[r + 1] * p[r - l + 1] % MOD : 0));
-    ll hash2 =
-      (hi2[l] -
-       (r + 1 < n ? hi2[r + 1] * p2[r - l + 1] % MOD2 : 0));
-    return {(hash < 0 ? hash + MOD : hash),
-            (hash2 < 0 ? hash2 + MOD2 : hash2)};
-  }
+		hi[n - 1] = hi2[n - 1] = s[n - 1];
+		for (int i = n - 2; i >= 0; i--)
+			hi[i] = (s[i] + hi[i + 1] * P) % MOD, hi2[i] = (s[i] + hi2[i + 1] * P) % MOD2;
+	}
+	pll query(int l, int r) {
+		ll hash = (h[r] - (l ? h[l - 1] * p[r - l + 1] % MOD : 0));
+		ll hash2 = (h2[r] - (l ? h2[l - 1] * p2[r - l + 1] % MOD2 : 0));
+
+		return { (hash < 0 ? hash + MOD : hash), (hash2 < 0 ? hash2 + MOD2 : hash2) };
+	}
+	pll query_inv(int l, int r) {
+		ll hash = (hi[l] - (r + 1 < n ? hi[r + 1] * p[r - l + 1] % MOD : 0));
+		ll hash2 = (hi2[l] - (r + 1 < n ? hi2[r + 1] * p2[r - l + 1] % MOD2 : 0));
+		return { (hash < 0 ? hash + MOD : hash), (hash2 < 0 ? hash2 + MOD2 : hash2) };
+	}
 };
