639d8e52fee0822c011403213cbb58510b0bc810
iagorrr <iagorrr04@gmail.com>
2024-04-13 19:58:20 -0300

    :white_check_mark: strings

--- submissions/atCoder/abc349_b.cpp
+++ submissions/atCoder/abc349_b.cpp
@@ -4,29 +4,22 @@ using namespace std;
 auto run() {
 	string s;
 	cin >> s;
-	string t;
-	cin >> t;
-
-	s.push_back('x');
-
-	char a = tolower(t[0]);
-	char b = tolower(t[1]);
-	char c = tolower(t[2]);
-
-	string s2;
-	for (auto c2 : s) {
-		if (c2 != a and c2 != b and c2 != c) continue;
-		s2.push_back(c2);
+	
+	map<char, int> h;
+	for (auto c : s) {
+		h[c]++;
 	}
 
-	int cnt = 0;
-	for (auto c2 : s) {
-		if (cnt == 0 and c2 == a) cnt++;
-		else if (cnt == 1 and c2 == b) cnt++;
-		else if (cnt == 2 and c2 == c) cnt++;
+	bool ans = 1;
+	for (int i = 1; i <= 2*(int)s.size(); i++) {
+		int k = 0;
+		for (auto [_, q] : h) k += (q == i);
+		ans &= (k == 2 or k == 0);
+		//cerr << i << ' ' << k << ' ' << ans << '\n';
 	}
 
-	cout << (cnt == 3 ? "Yes" : "No") << '\n';
+	cout << (ans ? "Yes" : "No") << '\n';
+
 }
 
 int main() {
