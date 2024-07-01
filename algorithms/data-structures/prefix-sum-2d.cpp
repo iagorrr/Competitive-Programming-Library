a27c5689ee4176d89bbf85546df24c0f7509686c
iagorrr <iagorrr@users.noreply.github.com>
2024-07-01 14:30:16 +0000

    updates notebok

--- algorithms/data-structures/prefix-sum-2d.cpp
+++ algorithms/data-structures/prefix-sum-2d.cpp
@@ -3,7 +3,7 @@ struct psum2d {
   vector<vector<T>> s;
   vector<vector<T>> psum;
   psum2d(vector<vector<T>> &grid, int n, int m)
-    : s(n + 1, vll(m + 1)), psum(n + 1, vll(m + 1)) {
+    : s(n + 1, vector<T>(m + 1)), psum(n + 1, vector<T>(m + 1)) {
     for (int i = 1; i <= n; i++)
       for (int j = 1; j <= m; j++) {
         s[i][j] = s[i][j - 1] + grid[i - 1][j - 1];
