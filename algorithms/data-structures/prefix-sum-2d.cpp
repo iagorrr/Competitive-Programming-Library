9cfc0f43a1a1ff2125792e95e4b8a9aed659e300
iagorrr <iagorrr04@gmail.com>
2024-07-01 11:29:10 -0300

    fix prefix sum 2d indices and types

--- algorithms/data-structures/prefix-sum-2d.cpp
+++ algorithms/data-structures/prefix-sum-2d.cpp
@@ -1,21 +1,22 @@
-
+template<typename T>
 struct psum2d {
-  vll2d s;
-  vll2d psum;
-  psum2d(vll2d &grid, int n, int m)
+  vector<vector<T>> s;
+  vector<vector<T>> psum;
+  psum2d(vector<vector<T>> &grid, int n, int m)
     : s(n + 1, vll(m + 1)), psum(n + 1, vll(m + 1)) {
     for (int i = 1; i <= n; i++)
-      for (int j = 1; j <= m; j++)
-        s[i][j] = s[i][j - 1] + grid[i][j];
-
-    for (int i = 1; i <= n; i++)
-      for (int j = 1; j <= m; j++)
+      for (int j = 1; j <= m; j++) {
+        s[i][j] = s[i][j - 1] + grid[i-1][j-1];
         psum[i][j] = psum[i - 1][j] + s[i][j];
+      }
+
   }
 
-  ll query(int x1, int y1, int x2, int y2) {
-    ll ans = psum[x2][y2] + psum[x1 - 1][y1 - 1];
-    ans -= psum[x2][y1 - 1] + psum[x1 - 1][y2];
+  T query(int x1, int y1, int x2, int y2) {
+    T ans = psum[x2+1][y2+1] + psum[x1][y1];
+    ans -= psum[x2+1][y1] + psum[x1][y2+1];
     return ans;
   }
 };
+
+
