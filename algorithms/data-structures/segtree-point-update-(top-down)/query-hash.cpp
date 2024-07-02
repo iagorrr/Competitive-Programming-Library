7a69ab045cff76222d2958ffd908884705b7264f
iagorrr <iagorrr04@gmail.com>
2024-07-02 15:57:39 -0300

    updates segment tree top down query hash and adds tests

--- algorithms/data-structures/segtree-point-update-(top-down)/query-hash.cpp
+++ algorithms/data-structures/segtree-point-update-(top-down)/query-hash.cpp
@@ -1,5 +1,8 @@
-const int MAXN(1'000'000);
-const ll MOD(1'000'000'009), P(31);
+using ll = long long;
+
+const int MAXN = 1'000'000;
+const ll MOD = 1'000'000'009, P(31);
+
 ll pows[MAXN + 1];
 void computepows() {
   pows[0] = 1;
@@ -26,3 +29,47 @@ inline Node combine(Node &vl, Node &vr, int nl, int nr,
     (vl.hash + ((vr.hash * pows[lsize]) % MOD)) % MOD;
   return vm;
 }
+
+
+template<typename T=Node, auto F=combine>
+struct SegTree {
+  int n;
+  vector<T> st;
+  SegTree(int n) : n(n), st(n << 2) {}
+
+  void assign(int p, const T &v) {
+    assign(1, 0, n - 1, p, v);
+  }
+  void assign(int node, int l, int r, int p,
+              const T &v) {
+    if (l == r) {
+      st[node] = v;
+      return;
+    }
+
+    int m = midpoint(l, r);
+    if (p <= m)
+      assign(node << 1, l, m, p, v);
+    else
+      assign(node << 1 | 1, m + 1, r, p, v);
+
+    st[node] =
+      F(st[node << 1], st[node << 1 | 1], l, r, l, r);
+  }
+
+  inline T query(int l, int r) {
+    return query(1, 0, n - 1, l, r);
+  }
+
+  inline T query(int node, int nl, int nr, int l,
+                    int r) const {
+    if (r < nl or nr < l) return T();
+    if (l <= nl and nr <= r) return st[node];
+
+    int m = midpoint(nl, nr);
+    auto a = query(node << 1, nl, m, l, r);
+    auto b = query(node << 1 | 1, m + 1, nr, l, r);
+
+    return F(a, b, nl, nr, l, r);
+  }
+};
