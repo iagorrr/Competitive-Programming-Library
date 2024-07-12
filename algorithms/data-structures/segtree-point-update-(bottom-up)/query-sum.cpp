fdd87081a458b8a173dcf0c16c66d178da0573a6
iagorrr <iagorrr04@gmail.com>
2024-07-12 15:38:12 -0300

    adds tests for segment tree bottom up query sum

--- algorithms/data-structures/segtree-point-update-(bottom-up)/query-sum.cpp
+++ algorithms/data-structures/segtree-point-update-(bottom-up)/query-sum.cpp
@@ -1,11 +1,33 @@
+using ll = long long;
+
 struct Node {
   ll value;
   Node() : value(0){};  // Neutral element
   Node(ll v) : value(v){};
 };
 
-Node combine(Node &nl, Node &nr) {
+inline Node combine(const Node &nl, const Node &nr) {
   Node m;
   m.value = nl.value + nr.value;
   return m;
 }
+
+struct SegTree {
+  int n;
+  vector<Node> st;
+  SegTree(int _n) : n(_n), st(n << 1) {}
+
+  void assign(int p, const Node &k) {
+    for (st[p += n] = k; p >>= 1;)
+      st[p] = combine(st[p << 1], st[p << 1 | 1]);
+  }
+
+  Node query(int l, int r) {
+    Node ansl, ansr;
+    for (l += n, r += n + 1; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) ansl = combine(ansl, st[l++]);
+      if (r & 1) ansr = combine(st[--r], ansr);
+    }
+    return combine(ansl, ansr);
+  }
+};
