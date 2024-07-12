5902dd90b398445057d69c3e3c3e2df3e5489b52
iagorrr <iagorrr04@gmail.com>
2024-07-12 15:36:47 -0300

    fix ERR string in test algorithm script

--- scripts/test-algorithm/script.sh
+++ scripts/test-algorithm/script.sh
@@ -14,7 +14,8 @@ do
 		{ \time -f "time: %E memory: %M" ./main  < $infile > out.txt ;} 2>&1 | tr -d '\n'
 
 		if ! diff -q  out.txt "$entry"/out/$filename &>/dev/null; then
-			>&2 echo  "ERR"
+			>&2 echo  " ERR"
+			diff out.txt "$entry"/out/$filename 
 			break
 		fi
 		echo -e " OK"
