6668b2c2bf0b64e4eb886fb50e57d04a38c4aeda
iagorrr <iagorrr04@gmail.com>
2024-08-03 20:30:30 -0300

    fix missing " in bash variable

--- scripts/test-algorithm/script.sh
+++ scripts/test-algorithm/script.sh
@@ -11,7 +11,7 @@ do
 
 		filename="${infile##*/}"
 		echo -en "\t\tinput: $filename " 
-		{ \time -f "time: %E memory: %M" ./main  < $infile > out.txt ;} 2>&1 | tr -d '\n'
+		{ \time -f "time: %E memory: %M" ./main  < "$infile" > out.txt ;} 2>&1 | tr -d '\n'
 
 		if ! diff -q  out.txt "$entry"/out/$filename &>/dev/null; then
 			>&2 echo  " ERR"
