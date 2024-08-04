3fe2416f96aff5b241019d243bff15b914996a3a
iagorrr <iagorrr04@gmail.com>
2024-08-03 21:13:03 -0300

    updates macro

--- settings-and-macros/macro.cpp
+++ settings-and-macros/macro.cpp
@@ -1,59 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 #ifdef LOCAL
 #include "debug.cpp"
 #else
 #define dbg(...)
 #endif
+
 #define endl '\n'
+
 #define fastio                  \
   ios_base::sync_with_stdio(0); \
   cin.tie(0);
-// #define int long long
-#define len(__x) (int)__x.size()
-using ll = long long;
-using ull = unsigned long long;
-using ld = long double;
-using vll = vector<ll>;
-using pll = pair<ll, ll>;
-using vll2d = vector<vll>;
-using vi = vector<int>;
-using vi2d = vector<vi>;
-using pii = pair<int, int>;
-using vii = vector<pii>;
-using vc = vector<char>;
-#define all(a) a.begin(), a.end()
-#define rall(a) a.rbegin(), a.rend()
-#define pb push_back
-#define eb emplace_back
-#define ff first
-#define ss second
-#define rep(i, begin, end)                              \
-  for (__typeof(begin) i = (begin) - ((begin) > (end)); \
-       i != (end) - ((begin) > (end));                  \
-       i += 1 - 2 * ((begin) > (end)))
 
-int lg2(ll x) {
-  return __builtin_clzll(1) - __builtin_clzll(x);
-}
+#define int long long
 
-// vector<string> dir({"LU", "U", "RU", "R", "RD", "D",
-// "LD", "L"}); int dx[] = {-1, -1, -1, 0, 1, 1, 1, 0}; int
-// dy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
-vector<string> dir({"U", "R", "D", "L"});
-int dx[] = {-1, 0, 1, 0};
-int dy[] = {0, 1, 0, -1};
+#define All(a) a.begin(), a.end()
+#define Rall(a) a.rbegin(), a.rend()
+#define For(i, a, b) for (common_type_t<decltype(a), decltype(b)> i = a; i != b; (a < b) ? ++i : --i)
+#define Sz(x) (int)x.size()
+#define Pb push_back
+#define Eb emplace_back 
 
-const ll OO = 1e18;
-const int oo = 1e9;
+using Ll = long long;
+using Ull = unsigned long long;
+using Ld = long double;
+using Vll = vector<ll>;
+using Pll = pair<ll, ll>;
+using Vll2d = vector<vll>;
+using Vi = vector<int>;
+using Vi2d = vector<vi>;
+using Pii = pair<int, int>;
+using Vii = vector<pii>;
+using Vc = vector<char>;
 
-int T(1);
+int T = 1;
 
 auto run() {
 
 }
 
-int32_t main(void) {
+int32_t main() {
 #ifndef LOCAL
   fastio;
 #endif
