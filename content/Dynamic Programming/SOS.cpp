951bc4afd816c9bbbed723816a8b9d1e343f4251
iagorrr <iagorrr04@gmail.com>
2024-08-26 13:40:09 -0300

    updates SOS dp and adds description

--- content/Dynamic Programming/SOS.cpp
+++ content/Dynamic Programming/SOS.cpp
@@ -1,9 +1,27 @@
-const int LOG = 17;
-int dp[(1<< LOG)];
-for(int b = 0; b < LOG; b++){
-    for(int mask = 0; mask < (1<< LOG); mask++){
-        if(mask & (1<< b)){
-            dp[mask] |= dp[mask ^ (1LL << b)];
-        }
+/*8<
+  @Title: Sum of Subsets
+  @Description:
+    Allows you to find if some mask $X$ is a
+    super mask of any of the given masks
+  @Usage:
+    Call $build$ with the $masks$ then it returns
+    a vector of bool $V$ where $V_X$ says if $X$
+    is a super mask of any of the initial maks
+  @Time: $O(LOG^2 \cdot 2^{LOG})$
+  @Memory: $O(LOG^2 \cdot 2^{LOG})$
+  @Warning:
+    Remember to set $LOG$ with the highest
+    bit possible
+>8*/
+const int LOG = 20;
+vc build(const vi &masks) {
+  vc ret(1 << LOG);
+  trav(mi, masks) ret[mi] = 1;
+  rep(b, 0, LOG) {
+    rep(mask, 0, (1 << LOG)) {
+      if (mask & (1 << b))
+        ret[mask] |= ret[mask ^ (1 << b)];
     }
+  }
+  return ret;
 }
