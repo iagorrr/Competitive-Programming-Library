2a02ccecfc4a05409be8c88a31f649c06c7a09b2
iagorrr <iagorrr04@gmail.com>
2024-09-13 00:46:21 -0300

    adds convex hull

--- content/Geometry/Convex hull.cpp
+++ content/Geometry/Convex hull.cpp
@@ -1,57 +1,17 @@
-struct pt {
-  double x, y;
-  int id;
-};
+#include "../Contest/template.cpp"
+#include "./Determinant.cpp"
+#include "./template.cpp"
 
-int orientation(pt a, pt b, pt c) {
-  double v = a.x * (b.y - c.y) +
-             b.x * (c.y - a.y) +
-             c.x * (a.y - b.y);
-  if (v < 0) return -1;  // clockwise
-  if (v > 0) return +1;  // counter-clockwise
-  return 0;
-}
-
-bool cw(pt a, pt b, pt c,
-        bool include_collinear) {
-  int o = orientation(a, b, c);
-  return o < 0 || (include_collinear && o == 0);
-}
-bool collinear(pt a, pt b, pt c) {
-  return orientation(a, b, c) == 0;
-}
-
-void convex_hull(vector<pt> &pts,
-                 bool include_collinear = false) {
-  pt p0 = *min_element(all(pts), [](pt a, pt b) {
-    return make_pair(a.y, a.x) <
-           make_pair(b.y, b.x);
-  });
-  sort(all(pts), [&p0](const pt &a, const pt &b) {
-    int o = orientation(p0, a, b);
-    if (o == 0)
-      return (p0.x - a.x) * (p0.x - a.x) +
-                 (p0.y - a.y) * (p0.y - a.y) <
-             (p0.x - b.x) * (p0.x - b.x) +
-                 (p0.y - b.y) * (p0.y - b.y);
-    return o < 0;
-  });
-  if (include_collinear) {
-    int i = len(pts) - 1;
-    while (i >= 0 &&
-           collinear(p0, pts[i], pts.back()))
-      i--;
-    reverse(pts.begin() + i + 1, pts.end());
-  }
-
-  vector<pt> st;
-  for (int i = 0; i < len(pts); i++) {
-    while (st.size() > 1 &&
-           !cw(st[len(st) - 2], st.back(), pts[i],
-               include_collinear))
-      st.pop_back();
-    st.push_back(pts[i]);
-  }
-
-  pts = st;
+template <typename T>
+vector<Point<T>> convexHull(vector<Point<T>> pts) {
+  if (len(pts) <= 1) return pts;
+  sort(all(pts));
+  vector<Point<T>> h(len(pts) + 1);
+  int s = 0, t = 0;
+  for (int it = 2; it--; s = --t, reverse(all(pts)))
+    for (Point<T> p : pts) {
+      while (t >= s + 2 && determinant(h[t - 2], h[t - 1], p) <= 0) t--;
+      h[t++] = p;
+    }
+  return {h.begin(), h.begin() + t - (t == 2 && h[0] == h[1])};
 }
