1fe320e4a5d6799e7d51e80662f804c15443b744
iagorrr <iagorrr04@gmail.com>
2024-09-20 17:38:36 -0300

    updates segment tree query sum

--- content/Data Structures/Segment tree point update range query/Query sum (bottom up).cpp
+++ content/Data Structures/Segment tree point update range query/Query sum (bottom up).cpp
@@ -4,8 +4,7 @@ struct Node {
   Node(ll v) : value(v){};
 };
 
-inline Node combine(const Node &nl,
-                    const Node &nr) {
+inline Node combine(const Node &nl, const Node &nr) {
   Node m;
   m.value = nl.value + nr.value;
   return m;
@@ -17,14 +16,12 @@ struct SegTree {
   SegTree(int _n) : n(_n), st(n << 1) {}
 
   void assign(int p, const Node &k) {
-    for (st[p += n] = k; p >>= 1;)
-      st[p] = combine(st[p << 1], st[p << 1 | 1]);
+    for (st[p += n] = k; p >>= 1;) st[p] = combine(st[p << 1], st[p << 1 | 1]);
   }
 
   Node query(int l, int r) {
     Node ansl = Node(), ansr = Node();
-    for (l += n, r += n + 1; l < r;
-         l >>= 1, r >>= 1) {
+    for (l += n, r += n + 1; l < r; l >>= 1, r >>= 1) {
       if (l & 1) ansl = combine(ansl, st[l++]);
       if (r & 1) ansr = combine(st[--r], ansr);
     }
