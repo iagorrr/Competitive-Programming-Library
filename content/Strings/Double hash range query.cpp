e43578b52c19cbbd726d286f60661f2cc4d22c8f
iagorrr <iagorrr04@gmail.com>
2026-03-26 11:09:24 -0300

    fix double hash range query by using distinct primes for powers

--- content/Strings/Double hash range query.cpp
+++ content/Strings/Double hash range query.cpp
@@ -1,35 +1,44 @@
+/*
+Good values for P1/P2, according to the size of N
+
+26: 29, 31
+10^1: 11, 13
+10^2: 101, 103
+10^3: 1009, 1013
+10^5: 100003, 100037
+10^6: 1000003, 1000033
+10^7: 10000019, 10000079
+10^8: 100000007, 100000037
+*/
+
 #include "../Contest/template.cpp"
-
-using ll = long long;
-using vll = vector<ll>;
-using pll = pair<ll, ll>;
-
 const int MAXN(1'000'000);
 const ll MOD = 1000027957;
 const ll MOD2 = 1000015187;
-const ll P = 31;
+const ll P1 = 29;
+const ll P2 = 31;
 
 ll p[MAXN + 1], p2[MAXN + 1];
 void precompute() {
     p[0] = p2[0] = 1;
     for (int i = 1; i <= MAXN; i++)
-        p[i] = (P * p[i - 1]) % MOD, p2[i] = (P * p2[i - 1]) % MOD2;
+        p[i] = (P1 * p[i - 1]) % MOD, p2[i] = (P2 * p2[i - 1]) % MOD2;
 }
 
 struct Hash {
     int n;
     vll h, h2, hi, hi2;
     Hash() {}
-    Hash(const string &s) : n(s.size()), h(n), h2(n), hi(n), hi2(n) {
+    Hash(const vll &s) : n(s.size()), h(n), h2(n), hi(n), hi2(n) {
         h[0] = h2[0] = s[0];
         for (int i = 1; i < n; i++)
-            h[i] = (s[i] + h[i - 1] * P) % MOD,
-            h2[i] = (s[i] + h2[i - 1] * P) % MOD2;
+            h[i] = (s[i] + h[i - 1] * P1) % MOD,
+            h2[i] = (s[i] + h2[i - 1] * P2) % MOD2;
 
         hi[n - 1] = hi2[n - 1] = s[n - 1];
         for (int i = n - 2; i >= 0; i--)
-            hi[i] = (s[i] + hi[i + 1] * P) % MOD,
-            hi2[i] = (s[i] + hi2[i + 1] * P) % MOD2;
+            hi[i] = (s[i] + hi[i + 1] * P1) % MOD,
+            hi2[i] = (s[i] + hi2[i + 1] * P2) % MOD2;
     }
     pll query(int l, int r) {
         ll hash = (h[r] - (l ? h[l - 1] * p[r - l + 1] % MOD : 0));
