4058553e969a26711242075ca69f54643f588bb2
iagorrr <iagorrr04@gmail.com>
2026-07-21 08:14:17 -0300

    Add functional graph struct and decomposition

---
+++ content/Graphs/functional-graph/functional-graph.cpp
@@ -0,0 +1,9 @@
+
+#include "../../Contest/template.cpp"
+
+struct FunctionalGraph {
+    vector<int> next;
+    vector<int> cycleIds;
+    vector<int> cycleSizes;
+    vector<int> distanceToCycle;
+};
