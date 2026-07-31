6ccca593033fd836b7dba2531790a6cb93fd4d44
iagorrr <iagorrr04@gmail.com>
2026-07-30 23:33:55 -0300

    Update SCC implementation

--- content/Graphs/Strongly connected components.cpp
+++ content/Graphs/Strongly connected components.cpp
@@ -13,27 +13,30 @@
 
     Build: $O(V+E)$
 >8*/
+#include "../Contest/template.cpp"
 
 struct SCC {
     int n, num_sccs;
-    vi2d adj;
-    vi scc_id;
+    vector<vector<int>> adj;
+    vector<int> scc_id;
 
-    SCC(int _n) : n(_n), num_sccs(0), adj(n), scc_id(n, -1) {}
+    SCC(const vector<vector<int>> &_adj)
+        : n(_adj.size()), num_sccs(0), adj(_adj), scc_id(n, -1) {
+        build();
+    }
 
-    void add_edge(int u, int v) { adj[u].eb(v); }
-
-    void find_sccs() {
+    void build() {
         int timer = 1;
-        vi tin(n), st;
+        vector<int> tin(n), st;
         st.reserve(n);
         function<int(int)> dfs = [&](int u) -> int {
-            int low = tin[u] = timer++, siz = len(st);
+            int low = tin[u] = timer++, siz = st.size();
             st.eb(u);
             for (int v : adj[u])
                 if (scc_id[v] < 0) low = min(low, tin[v] ? tin[v] : dfs(v));
             if (tin[u] == low) {
-                rep(i, siz, len(st)) scc_id[st[i]] = num_sccs;
+                for (int i = siz; i < (int)st.size(); i++)
+                    scc_id[st[i]] = num_sccs;
                 st.resize(siz);
                 num_sccs++;
             }
@@ -42,20 +45,5 @@ struct SCC {
 
         for (int i = 0; i < n; i++)
             if (!tin[i]) dfs(i);
-    }
-
-    vector<set<int>> build_gscc() {
-        vector<set<int>> gscc;
-        for (int i = 0; i < len(adj); ++i)
-            for (auto j : adj[i])
-                if (scc_id[i] != scc_id[j]) gscc[scc_id[i]].emplace(scc_id[j]);
-        return gscc;
-    }
-
-    vi2d per_comp() {
-        vi2d ret(num_sccs);
-        rep(i, 0, n) ret[scc_id[i]].eb(i);
-        reverse(all(ret));  // already in topological order ;)
-        return ret;
     }
 };
